239f9c95331da949a7069ef364c4b2c3c4b30bac
Adam Sotona <asotona@openjdk.org>
2024-02-15 17:11:50 +0100

    more constrained TestLiftSmallCorpus

--- test/jdk/java/lang/reflect/code/bytecode/TestLiftSmallCorpus.java
+++ test/jdk/java/lang/reflect/code/bytecode/TestLiftSmallCorpus.java
@@ -73,7 +73,7 @@ public class TestLiftSmallCorpus {
         }
 
         // @@@ There is still several failing cases
-        Assert.assertTrue(failed < 5, STR."failed: \{failed}, passed: \{passed}, skipped: \{skipped}");
+        Assert.assertTrue(failed < 5 && passed > 4200, STR."failed: \{failed}, passed: \{passed}, skipped: \{skipped}");
     }
 
     private void testDoubleRoundtripStability(Path path) throws Exception {
