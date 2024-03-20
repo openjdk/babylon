422e3f9d72675a9d9bc91198ca8261e4eabbe4bc
Mourad Abbay <mourad.abbay@oracle.com>
2024-03-20 02:27:18 +0000

    Apply recommendations

--- test/jdk/java/lang/reflect/code/TestRemoveFinalVars.java
+++ test/jdk/java/lang/reflect/code/TestRemoveFinalVars.java
@@ -1,3 +1,4 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.Method;
@@ -5,8 +6,11 @@ import java.lang.reflect.code.Block;
 import java.lang.reflect.code.CopyContext;
 import java.lang.reflect.code.Op;
 import java.lang.reflect.code.Value;
+import java.lang.reflect.code.analysis.SSA;
+import java.lang.reflect.code.interpreter.Interpreter;
 import java.lang.runtime.CodeReflection;
 import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import static java.lang.reflect.code.op.CoreOps.FuncOp;
@@ -22,39 +26,55 @@ import static java.lang.reflect.code.op.CoreOps.VarOp;
 public class TestRemoveFinalVars {
 
     @CodeReflection
-    static int f(boolean b) {
+    static boolean f() {
         final int x = 8; // final var
         int y = x + 2; // final var
         int z = y + 3; // non final var
         z++;
-        return y;
+        return x == 8 && y == 10 && z == 14;
     }
 
     @Test
     void test() {
         FuncOp f = getFuncOp(this.getClass(),"f");
         f.writeTo(System.out);
+        FuncOp lf = lower(f);
+        lf.writeTo(System.out);
 
         FuncOp f2 = f.transform(TestRemoveFinalVars::rmFinalVars);
         f2.writeTo(System.out);
+        FuncOp lf2 = lower(f2);
+        lf2.writeTo(System.out);
+
+        Assert.assertEquals(Interpreter.invoke(lf), Interpreter.invoke(lf2));
+
+        SSA.transform(lower(f)).writeTo(System.out);
     }
 
-    /*
-    if VarOp
-        if varOp result not used with VarStore // final variable
-            do not copy the varOp
-            map varOp result with the initial value // a way to mark the var is final
-            do not copy VarLoads on the var and map their result with the init value
-
-    * */
+    static FuncOp lower(FuncOp funcOp) {
+        return funcOp.transform((block, op) -> {
+            if (op instanceof Op.Lowerable lop) {
+                return lop.lower(block);
+            } else {
+                block.op(op);
+                return block;
+            }
+        });
+    }
 
     static Block.Builder rmFinalVars(Block.Builder block, Op op) {
         if (op instanceof VarOp varOp) {
-            if (isValueUsedWithOpClass(varOp.result(), VarStoreOp.class)) {
+            // Is the variable stored to? If not we can remove it
+            // otherwise, it's not considered final and we copy it
+            if (isValueUsedWithOp(varOp.result(), o -> o instanceof VarStoreOp)) {
                 block.op(varOp);
             }
         } else if (op instanceof VarLoadOp varLoadOp) {
-            if (!isValueUsedWithOpClass(varLoadOp.varOp().result(), VarStoreOp.class)) {
+            // If the variable is not stored to
+            if (!isValueUsedWithOp(varLoadOp.varOp().result(), o -> o instanceof VarStoreOp)) {
+                // Map result of load from variable to the value that initialized the variable
+                // Subsequently encountered input operations using the result will be copied
+                // to output operations using the mapped value
                 CopyContext cc = block.context();
                 cc.mapValue(varLoadOp.result(), cc.getValue(varLoadOp.varOp().operands().get(0)));
             } else {
@@ -67,8 +87,12 @@ public class TestRemoveFinalVars {
     }
 
     private static boolean isValueUsedWithOpClass(Value value, Class<? extends Op> opClass) {
+        return isValueUsedWithOp(value, op -> opClass.isAssignableFrom(op.getClass()));
+    }
+
+    private static boolean isValueUsedWithOp(Value value, Predicate<Op> opPredicate) {
         for (Op.Result user : value.uses()) {
-            if (user.op().getClass().equals(opClass)) {
+            if (opPredicate.test(user.op())) {
                 return true;
             }
         }
