907aede934375cf9d3f7ddd79537f7bba4fb3846
Mourad Abbay <mourad.abbay@oracle.com>
2024-06-05 08:27:02 +0100

    Add missing import

--- test/jdk/java/lang/reflect/code/TestSwitchExpressionOp.java
+++ test/jdk/java/lang/reflect/code/TestSwitchExpressionOp.java
@@ -2,7 +2,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.Method;
-import java.lang.reflect.code.Op;
+import java.lang.reflect.code.OpTransformer;
 import java.lang.reflect.code.interpreter.Interpreter;
 import java.lang.reflect.code.op.CoreOp;
 import java.lang.runtime.CodeReflection;
