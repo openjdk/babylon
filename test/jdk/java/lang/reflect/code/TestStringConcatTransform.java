729de59de66393648d35db7106cc22d271e0eb8e
Ian Graves <ian.graves@oracle.com>
2024-06-13 17:19:25 -0500

    Updates and refactorings

--- test/jdk/java/lang/reflect/code/TestStringConcatTransform.java
+++ test/jdk/java/lang/reflect/code/TestStringConcatTransform.java
@@ -30,14 +30,12 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.NoInjection;
 import org.testng.annotations.Test;
-import java.lang.reflect.code.transformations.LinearConcatTransform;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
+import java.lang.reflect.code.OpTransformer;
+import java.lang.reflect.code.analysis.LinearStringContextTransformer;
+
 import java.lang.reflect.Method;
-import java.lang.reflect.code.Op;
 import java.lang.reflect.code.analysis.SSA;
-import java.lang.reflect.code.bytecode.BytecodeGenerator;
 import java.lang.reflect.code.interpreter.Interpreter;
 import java.lang.reflect.code.op.CoreOp;
 import java.lang.runtime.CodeReflection;
@@ -95,10 +93,9 @@ public class TestStringConcatTransform {
     @Test(dataProvider = "getClassMethods")
     public void testModelTransform(@NoInjection Method method) {
         CoreOp.FuncOp model = method.getCodeModel().orElseThrow();
-        CoreOp.FuncOp f_transformed = model.transform(new LinearConcatTransform.ConcatTransform());
+        CoreOp.FuncOp f_transformed = model.transform(new LinearStringContextTransformer());
         Object[] args = prepArgs(method);
 
-        System.out.println("Final Transformed");
         f_transformed.writeTo(System.out);
 
         var interpreted = Interpreter.invoke(model, args);
@@ -111,54 +108,24 @@ public class TestStringConcatTransform {
     @Test(dataProvider = "getClassMethods")
     public void testSSAModelTransform(@NoInjection Method method) {
         CoreOp.FuncOp model = method.getCodeModel().orElseThrow();
-        System.out.println("Basic model");
-        model.writeTo(System.out);
+        CoreOp.FuncOp transformed_model = model.transform(new LinearStringContextTransformer());
         CoreOp.FuncOp ssa_model = generateSSA(model);
-        System.out.println("Final Transformed");
-        CoreOp.FuncOp f_transformed = ssa_model.transform(new LinearConcatTransform.ConcatTransform());
-        f_transformed.writeTo(System.out);
+        CoreOp.FuncOp ssa_transformed_model = ssa_model.transform(new LinearStringContextTransformer());
         Object[] args = prepArgs(method);
 
-        var interpreted = Interpreter.invoke(model, args);
-        var transformed_interpreted = Interpreter.invoke(f_transformed, args);
+        var model_interpreted = Interpreter.invoke(model, args);
+        var transformed_model_interpreted = Interpreter.invoke(transformed_model, args);
         var ssa_interpreted = Interpreter.invoke(ssa_model, args);
+        var ssa_transformed_interpreted = Interpreter.invoke(ssa_transformed_model, args);
         try {
             var jvm_interpreted = method.invoke(null, args);
-            Assert.assertEquals(transformed_interpreted, jvm_interpreted);
-            //Assert.assertEquals(interpreted, transformed_interpreted);
+            Assert.assertEquals(model_interpreted, transformed_model_interpreted);
+            Assert.assertEquals(transformed_model_interpreted, ssa_interpreted);
+            Assert.assertEquals(ssa_interpreted, ssa_transformed_interpreted);
+            Assert.assertEquals(ssa_transformed_interpreted, jvm_interpreted);
         } catch (Throwable e) {
             throw new RuntimeException(e);
         }
-
-
-    }
-
-
-    //@Test(dataProvider = "getClassMethods")
-    public void testEqualResults(@NoInjection Method method) {
-        CoreOp.FuncOp f = method.getCodeModel().orElseThrow();
-        CoreOp.FuncOp f_transformed = f.transform(new LinearConcatTransform.ConcatTransform());
-        CoreOp.FuncOp f_ssa = generateSSA(f).transform(new LinearConcatTransform.ConcatTransform());
-        MethodHandle m_bytecode = generateBytecode(f_transformed);
-
-
-        Object[] args = prepArgs(method);
-
-        var interpreted = Interpreter.invoke(f, args);
-        var transformed_interpreted = Interpreter.invoke(f_transformed, args);
-        var transformed_ssa_interpreted = Interpreter.invoke(f_ssa, args);
-        try {
-            var jvm_executed = method.invoke(null, args);
-            var code_model_bytecode_executed = m_bytecode.invokeWithArguments(args);
-
-            //Assert.assertEquals(interpreted, transformed_interpreted);
-            //Assert.assertEquals(transformed_interpreted, jvm_executed);
-            //Assert.assertEquals(jvm_executed, transformed_ssa_interpreted);
-            //Assert.assertEquals(transformed_ssa_interpreted, code_model_bytecode_executed);
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
-        }
-
     }
 
     public static Object[] prepArgs(Method m) {
@@ -184,55 +151,14 @@ public class TestStringConcatTransform {
         for (int i = 0; i < ars.size(); i++) {
             res[i] = ars.get(i);
         }
-
         return res;
     }
 
     static CoreOp.FuncOp generateSSA(CoreOp.FuncOp f) {
-        CoreOp.FuncOp lf = f.transform((block, op) -> {
-            if (op instanceof Op.Lowerable lop) {
-                return lop.lower(block);
-            } else {
-                block.op(op);
-                return block;
-            }
-        });
-
+        CoreOp.FuncOp lf = f.transform(OpTransformer.LOWERING_TRANSFORMER);
         lf = SSA.transform(lf);
-
-        System.out.println("Post SSA");
         lf.writeTo(System.out);
-
         return lf;
-    }
-
-
-    static MethodHandle generateBytecode(CoreOp.FuncOp f) {
-        CoreOp.FuncOp lf = f.transform((block, op) -> {
-            if (op instanceof Op.Lowerable lop) {
-                return lop.lower(block);
-            } else {
-                block.op(op);
-                return block;
-            }
-        });
-
-        lf = SSA.transform(lf);
-        return BytecodeGenerator.generate(MethodHandles.lookup(), lf);
-    }
-
-    static CoreOp.FuncOp lower(CoreOp.FuncOp f) {
-        CoreOp.FuncOp lf = f.transform((block, op) -> {
-            if (op instanceof Op.Lowerable lop) {
-                return lop.lower(block);
-            } else {
-                block.op(op);
-                return block;
-            }
-        });
-
-        return lf;
-
     }
 
     @CodeReflection
