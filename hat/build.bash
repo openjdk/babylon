a70d3e942f3109278ef8c4ae5abf19d552e9395a
Gary Frost <gfrost@openjdk.org>
2024-06-15 15:15:40 +0000

    Switching to maven build

--- hat/build.bash
+++ hat/build.bash
@@ -1,10 +1,1 @@
-cmake -B build
-for backend in \
-   opencl\
-   mock\
-   ptx\
-;do
-    cmake --build build --target ${backend}_backend
-done
-
-mvn clean compile jar:jar
+mvn clean compile jar:jar install
