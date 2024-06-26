7d966a45393386e2b2349445a504e29ced3151c7
Ian Graves <ian.graves@oracle.com>
2024-06-26 12:35:15 -0500

    Integrating Paul's tree-based approach with argument handling

--- src/java.base/share/classes/java/lang/reflect/code/analysis/StringConcatTransformer.java
+++ src/java.base/share/classes/java/lang/reflect/code/analysis/StringConcatTransformer.java
@@ -40,130 +40,82 @@ import java.util.*;
  */
 public class StringConcatTransformer implements OpTransformer {
 
-    private static final JavaType J_L_BYTESTRING = JavaType.type(StringBuilder.class);
-    private static final JavaType J_L_CHARSEQUENCE = JavaType.type(CharSequence.class);
     private static final JavaType J_L_OBJECT = JavaType.type(Object.class);
-    private static final MethodRef SB_TO_STRING = MethodRef.method(J_L_BYTESTRING, "toString", JavaType.J_L_STRING);
+    private static final JavaType J_L_STRING_BUILDER = JavaType.type(StringBuilder.class);
 
-    final private Set<Value> StringBuilders = new HashSet<>();
-    final private Map<Integer, Value> IntConstants = new HashMap<>();
+    private static final MethodRef SB_TO_STRING_REF = MethodRef.method(
+            J_L_STRING_BUILDER, "toString", JavaType.J_L_STRING);
 
-    public StringConcatTransformer() {
-    }
+    public StringConcatTransformer() {}
 
-    private static boolean reusableResult(Op.Result r) {
-        if (r.uses().size() == 1) {
-            return r.uses().stream().allMatch((use) -> use.op() instanceof CoreOp.ConcatOp);
-        }
-        return false;
-    }
-
-    @Override public Block.Builder apply(Block.Builder builder, Op op) {
-        if (op instanceof CoreOp.ConcatOp cz) {
-
-            Value left = builder.context().getValue(cz.operands().get(0));
-            Value right = builder.context().getValue(cz.operands().get(1));
-            Op.Result result = cz.result();
-            if (reusableResult(result)) {
-                Value sb = stringBuilder(builder, left, right);
-                builder.context().mapValue(result, sb);
-            } else {
-                Value sb = stringBuilder(builder, left, right);
-                Value str = buildString(builder, sb);
-                builder.context().mapValue(result, str);
+    @Override
+    public Block.Builder apply(Block.Builder block, Op op) {
+        switch (op) {
+            case CoreOp.ConcatOp cz when isRootConcat(cz) -> {
+                // Create a string builder and build by traversing tree of operands
+                Op.Result builder = block.apply(CoreOp._new(FunctionType.functionType(J_L_STRING_BUILDER)));
+                buildFromTree(block, builder, cz);
+                // Convert to string
+                Value s = block.op(CoreOp.invoke(SB_TO_STRING_REF, builder));
+                block.context().mapValue(cz.result(), s);
             }
+            case CoreOp.ConcatOp cz -> {
+                // Process later when building from root concat
+            }
+            default -> block.op(op);
+        }
+        return block;
+    }
+
+    static boolean isRootConcat(CoreOp.ConcatOp cz) {
+        // Root of concat tree, zero uses, two or more uses,
+        // or one use that is not a subsequent concat op
+        Set<Op.Result> uses = cz.result().uses();
+        return uses.size() != 1 || !(uses.iterator().next().op() instanceof CoreOp.ConcatOp);
+    }
+
+    static void buildFromTree(Block.Builder block, Op.Result builder, CoreOp.ConcatOp cz) {
+        // Process concat op's operands from left to right
+        buildFromTree(block, builder, cz.operands().get(0));
+        buildFromTree(block, builder, cz.operands().get(1));
+    }
+
+    static void buildFromTree(Block.Builder block, Op.Result builder, Value v) {
+        if (v instanceof Op.Result r &&
+                r.op() instanceof CoreOp.ConcatOp cz &&
+                r.uses().size() == 1) {
+            // Node of tree, recursively traverse the operands
+            buildFromTree(block, builder, cz);
         } else {
-            builder.op(op);
+            // Leaf of tree, append value to builder
+            // Note leaf can be the result of a ConcatOp with multiple uses
+            block.op(append(block, builder, block.context().getValue(v)));
         }
-        return builder;
     }
 
-    //Uses StringBuilder and Immediate String Value
-    private Value buildString(Block.Builder builder, Value sb){
-        var toStringInvoke = CoreOp.invoke(SB_TO_STRING, sb);
-        return builder.apply(toStringInvoke);
+    private static Op append(Block.Builder block, Value builder, Value arg) {
+        return append(block, builder, arg, arg.type());
     }
 
-    private Value stringBuilder(Block.Builder builder, Value left, Value right) {
-        if (left.type().equals(J_L_BYTESTRING) && StringBuilders.contains(left)
-                || right.type().equals(J_L_BYTESTRING) && StringBuilders.contains(right)) {
-            var res = builder.op(append(builder, left, right));
-            StringBuilders.add(res);
-            return res;
+    private static Op append(Block.Builder block, Value builder, Value arg, TypeElement type) {
+        // Check if we need to widen unsupported integer types in the StringBuilder API
+        // Strings are fed in as-is, everything else given as an Object.
+        if (type instanceof PrimitiveType) {
+            if (List.of(JavaType.BYTE, JavaType.SHORT).contains(type)) {
+                Value widened = block.op(CoreOp.conv(JavaType.INT, arg));
+                MethodRef methodDesc = MethodRef.method(J_L_STRING_BUILDER, "append", J_L_STRING_BUILDER, JavaType.INT);
+                return CoreOp.invoke(methodDesc, builder, widened);
+            } else {
+                MethodRef methodDesc = MethodRef.method(J_L_STRING_BUILDER, "append", J_L_STRING_BUILDER, type);
+                return CoreOp.invoke(methodDesc, builder, arg);
+            }
+        } else if (type.equals(JavaType.J_L_STRING)) {
+            MethodRef methodDesc = MethodRef.method(J_L_STRING_BUILDER, "append", J_L_STRING_BUILDER, type);
+            return CoreOp.invoke(methodDesc, builder, arg);
         } else {
-            CoreOp.NewOp newBuilder = CoreOp._new(FunctionType.functionType(J_L_BYTESTRING));
-            Value sb = builder.op(newBuilder);
-            StringBuilders.add(sb);
-            var res = builder.op(append(builder, sb, left));
-            StringBuilders.add(res);
-            var res2 = builder.op(append(builder, res, right));
-            StringBuilders.add(res2);
-            return res2;
+            MethodRef methodDesc = MethodRef.method(J_L_STRING_BUILDER, "append", J_L_STRING_BUILDER, J_L_OBJECT);
+            return CoreOp.invoke(methodDesc, builder, arg);
         }
-    }
-
-    private Op append(Block.Builder builder, Value left, Value right) {
-        //Left argument is a blessed stringbuilder
-        if (left.type().equals(J_L_BYTESTRING) && StringBuilders.contains(left)) {
-            var rightType = right.type();
-            if (rightType.equals(J_L_BYTESTRING)) {
-                rightType = J_L_CHARSEQUENCE;
-                MethodRef leftMethodDesc = MethodRef.method(J_L_BYTESTRING, "append", J_L_BYTESTRING, rightType);
-                return CoreOp.invoke(leftMethodDesc, left, right);
-            }
-
-            if (rightType instanceof PrimitiveType) {
-                if (List.of(JavaType.BYTE, JavaType.SHORT).contains(rightType)) {
-                    Value widened = builder.op(CoreOp.conv(JavaType.INT, right));
-                    MethodRef methodDesc = MethodRef.method(J_L_BYTESTRING, "append", J_L_BYTESTRING, JavaType.INT);
-                    return CoreOp.invoke(methodDesc, left, widened);
-                } else {
-                    MethodRef methodDesc = MethodRef.method(J_L_BYTESTRING, "append", J_L_BYTESTRING, rightType);
-                    return CoreOp.invoke(methodDesc, left, right);
-                }
-            } else if (rightType.equals(JavaType.J_L_STRING)) {
-                MethodRef methodDesc = MethodRef.method(J_L_BYTESTRING, "append", J_L_BYTESTRING, rightType);
-                return CoreOp.invoke(methodDesc, left, right);
-            } else {
-                MethodRef methodDesc = MethodRef.method(J_L_BYTESTRING, "append", J_L_BYTESTRING, J_L_OBJECT);
-                return CoreOp.invoke(methodDesc, left, right);
-            }
-        } else if (right.type().equals(J_L_BYTESTRING) && StringBuilders.contains(right)) {
-            var leftType = left.type();
-            var zero = getConstant(builder, 0);
-            if (leftType.equals(J_L_BYTESTRING)) {
-                leftType = J_L_CHARSEQUENCE;
-                MethodRef leftMethodDesc = MethodRef.method(J_L_BYTESTRING, "insert", J_L_BYTESTRING, JavaType.INT, leftType);
-                return CoreOp.invoke(leftMethodDesc, right, zero, left);
-            }
-            if (leftType instanceof PrimitiveType) {
-                if (List.of(JavaType.BYTE, JavaType.SHORT).contains(leftType)) {
-                    Value widened = builder.op(CoreOp.conv(JavaType.INT, left));
-                    MethodRef methodDesc = MethodRef.method(J_L_BYTESTRING, "insert", J_L_BYTESTRING, JavaType.INT, JavaType.INT);
-                    return CoreOp.invoke(methodDesc, right, zero, widened);
-                } else {
-                    MethodRef methodDesc = MethodRef.method(J_L_BYTESTRING, "insert", J_L_BYTESTRING, JavaType.INT, leftType);
-                    return CoreOp.invoke(methodDesc, right, zero, left);
-                }
-            } else if (leftType.equals(JavaType.J_L_STRING)) {
-                MethodRef methodDesc = MethodRef.method(J_L_BYTESTRING, "insert", J_L_BYTESTRING, JavaType.INT, leftType);
-                return CoreOp.invoke(methodDesc, right, zero, left);
-            } else {
-                MethodRef methodDesc = MethodRef.method(J_L_BYTESTRING, "insert", J_L_BYTESTRING, JavaType.INT, J_L_OBJECT);
-                return CoreOp.invoke(methodDesc, right, zero, left);
-            }
-        }
-        throw new RuntimeException("append requires a blessed StringBuilder as one Value argument");
-    }
-
-    private Value getConstant(Block.Builder builder, int con) {
-        var val = IntConstants.get(con);
-        if (val == null) {
-            var constOp = CoreOp.constant(JavaType.INT, con);
-            val = builder.op(constOp);
-            IntConstants.put(0, val);
-        }
-        return val;
     }
 
 }
