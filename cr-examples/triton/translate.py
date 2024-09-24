33bbdcf8f61ffc1519452bdbbe9c8dbf5d0c7fcb
hanklo6 <hank.lo@intel.com>
2024-09-24 11:26:55 -0700

    Add triton translator

---
+++ cr-examples/triton/translate.py
@@ -0,0 +1,31 @@
+import torch
+import intel_extension_for_pytorch  # type: ignore # noqa: F401
+
+import triton
+import triton.language as tl
+import shutil
+import os
+
+# declare the dummy functions
+@triton.jit
+def add_kernel():
+   a = 0
+
+@triton.jit
+def matmul_kernel():
+   a = 0
+
+@triton.jit
+def softmax_kernel():
+   a = 0
+
+ADD_KERNEL_MLIR = "/home/intel/babylon/cr-examples/triton/result/add_kernel"
+MATMUL_MLIR = "/home/intel/babylon/cr-examples/triton/result/matmul_kernel"
+SOFTMAX_MLIR = "/home/intel/babylon/cr-examples/triton/result/softmax_kernel"
+
+if os.path.isdir('/home/intel/.triton/cache'):
+   shutil.rmtree('/home/intel/.triton/cache')
+
+triton.compile(triton.compiler.ASTSource(fn=add_kernel, signature={}, constants={}), target_mlir=ADD_KERNEL_MLIR)
+triton.compile(triton.compiler.ASTSource(fn=softmax_kernel, signature={}, constants={}), target_mlir=SOFTMAX_MLIR, options={"num_warps":32})
+triton.compile(triton.compiler.ASTSource(fn=matmul_kernel, signature={}, constants={}), target_mlir=MATMUL_MLIR, options={"threads_per_warp":16, "num_warps":64})
