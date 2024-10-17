00b45c8ef7d2d9ae1a47cd097d8645154674f67b
Paul Sandoz <paul.d.sandoz@googlemail.com>
2024-10-17 15:50:16 -0700

    Remove debug stuff

--- test/langtools/tools/javac/reflect/CodeReflectionTester.java
+++ test/langtools/tools/javac/reflect/CodeReflectionTester.java
@@ -90,7 +90,6 @@ public class CodeReflectionTester {
             }
         } else if (Quotable.class.isAssignableFrom(field.getType())) {
             Quotable quotable = (Quotable) field.get(null);
-            System.out.println("XXX: " + quotable.quoted());
             String found = canonicalizeModel(field, getModelOfQuotedOp(quotable.quoted()));
             String expected = canonicalizeModel(field, ir.value());
             if (!found.equals(expected)) {
