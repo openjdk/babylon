c089532732a6fbef2568791ef50755d149e2c043
grfrost <frost.gary@gmail.com>
2024-10-22 18:04:45 +0100

    Simplified maven copy to build dir

--- hat/backends/spirv/scripts/build-beehive-spirv-toolkit.sh
+++ hat/backends/spirv/scripts/build-beehive-spirv-toolkit.sh
@@ -2,4 +2,4 @@ git clone https://github.com/beehive-lab/beehive-spirv-toolkit.git
 cd beehive-spirv-toolkit
 mvn clean install
 cd ..
-cp beehive-spirv-toolkit/lib/target/beehive-spirv-lib-0.0.4.jar ../../../maven-build/
+cp beehive-spirv-toolkit/lib/target/beehive-spirv-lib-0.0.4.jar ../../../build/
