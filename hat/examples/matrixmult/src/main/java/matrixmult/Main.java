c9e8b09b496ceeeabc7aeafb221367e5d96f85c4
Juan Fumero <jjfumero@gmail.com>
2024-11-15 15:18:02 +0100

    Matrix Multiplication Example added

---
+++ hat/examples/matrixmult/src/main/java/matrixmult/Main.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package matrixmult;
+
+import hat.Accelerator;
+import hat.ComputeContext;
+import hat.KernelContext;
+import hat.backend.Backend;
+import hat.buffer.F32Array;
+
+import jdk.incubator.code.CodeReflection;
+
+import java.util.Random;
+
+public class Main {
+
+    @CodeReflection
+    public static void matrixMultiplyKernel(KernelContext kc, F32Array matrixA, F32Array matrixB, F32Array matrixC, int size) {
+        if (kc.x < kc.maxX) {
+            for (int j = 0; j < size; j++) {
+                float acc = 0;
+                for (int k = 0; k < size; k++) {
+                    float a = matrixA.array(kc.x * size + k);
+                    float b = matrixB.array(k * size + j);
+                    acc += (a * b);
+                }
+                matrixC.array(kc.x * size + j, acc);
+            }
+        }
+    }
+
+    @CodeReflection
+    public static void matrixMultiply(ComputeContext cc, F32Array matrixA, F32Array matrixB, F32Array matrixC, int size) {
+        cc.dispatchKernel(size,
+                kc -> matrixMultiplyKernel(kc, matrixA, matrixB, matrixC, size)
+        );
+    }
+
+    private static void runSequential(F32Array matrixA, F32Array matrixB, F32Array matrixC, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int sum = 0;
+                for (int k = 0; k < size; k++) {
+                    float a = matrixA.array(i * size + k);
+                    float b = matrixB.array(k * size + j);
+                    sum += (a * b);
+                }
+                matrixC.array(i * size + j, sum);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running Matrix Multiplication!");
+
+        var lookup = java.lang.invoke.MethodHandles.lookup();
+        var accelerator = new Accelerator(lookup, Backend.FIRST);
+
+        final int size = 1024;
+        var matrixA = F32Array.create(accelerator, size * size);
+        var matrixB = F32Array.create(accelerator, size * size);
+
+        // Matrix for the result
+        var matrixC = F32Array.create(accelerator, size * size);
+
+        var resultSeq = F32Array.create(accelerator, size * size);
+
+        // Initialize matrices (A and B have the same size)
+        Random r = new Random(19);
+        for (int i = 0; i < matrixA.length(); i++) {
+            matrixA.array(i, r.nextFloat());
+            matrixB.array(i, r.nextFloat());
+        }
+
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            accelerator.compute(cc ->
+                    Main.matrixMultiply(cc, matrixA, matrixB, matrixC, size)
+            );
+            long end = System.nanoTime();
+            System.out.println("Elapsed Time: " + (end - start) + " ns");
+        }
+
+        // Check result
+        runSequential(matrixA, matrixB, resultSeq, size);
+        boolean isCorrect = true;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (Math.abs(matrixC.array(i * size + j) - matrixC.array(i * size + j)) > 0.1) {
+                    isCorrect = false;
+                }
+            }
+        }
+
+        if (isCorrect) {
+            System.out.println("Result is correct!");
+        } else {
+            System.out.println("Result is wrong!");
+        }
+
+    }
+}
