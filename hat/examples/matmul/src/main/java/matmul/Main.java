aa8e17698b2887e295aa99e59236696da1d31ee6
Juan Fumero <jjfumero@gmail.com>
2024-11-19 10:56:19 +0100

    MatrixMult example moved to matmul directory

--- hat/examples/matmul/src/main/java/matmul/Main.java
+++ hat/examples/matmul/src/main/java/matmul/Main.java
@@ -22,7 +22,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package matrixmult;
+package matmul;
 
 import hat.Accelerator;
 import hat.ComputeContext;
