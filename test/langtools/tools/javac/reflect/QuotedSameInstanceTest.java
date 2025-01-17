1613b2abe38097cb038c65decd88f5b1c535e275
Mourad Abbay <mourad.abbay@oracle.com>
2025-01-17 02:21:07 +0100

    Test that Op#ofQuotable returns same object for Quotable created via interpreter

--- test/langtools/tools/javac/reflect/QuotedSameInstanceTest.java
+++ test/langtools/tools/javac/reflect/QuotedSameInstanceTest.java
@@ -1,8 +1,13 @@
+import jdk.incubator.code.CodeReflection;
 import jdk.incubator.code.Op;
+import jdk.incubator.code.interpreter.Interpreter;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import jdk.incubator.code.Quotable;
+
+import java.lang.invoke.MethodHandles;
+import java.util.function.IntSupplier;
 import java.util.function.IntUnaryOperator;
 import java.util.stream.IntStream;
 
@@ -29,6 +34,24 @@ public class QuotedSameInstanceTest {
     @Test
     void testWithMultiThreads() {
         Object[] quotedObjects = IntStream.range(0, 1024).parallel().mapToObj(__ -> Op.ofQuotable(q2).get()).toArray();
+        for (int i = 1; i < quotedObjects.length; i++) {
+            Assert.assertSame(quotedObjects[i], quotedObjects[i - 1]);
+        }
+    }
+
+    public interface QuotableIntSupplier extends IntSupplier, Quotable {}
+    @CodeReflection
+    static Quotable q() {
+        QuotableIntSupplier r = () -> 8;
+        return r;
+    }
+
+    @Test
+    void testMultiThreadsViaInterpreter() throws NoSuchMethodException {
+        var qm = this.getClass().getDeclaredMethod("q");
+        var q = Op.ofMethod(qm).get();
+        Quotable quotable = (Quotable) Interpreter.invoke(MethodHandles.lookup(), q);
+        Object[] quotedObjects = IntStream.range(0, 1024).parallel().mapToObj(__ -> Op.ofQuotable(quotable).get()).toArray();
         for (int i = 1; i < quotedObjects.length; i++) {
             Assert.assertSame(quotedObjects[i], quotedObjects[i - 1]);
         }
