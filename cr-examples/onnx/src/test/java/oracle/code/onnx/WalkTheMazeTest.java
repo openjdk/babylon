b098587a93893792fa7607a2c9bbcb22390b3b73
Adam Sotona <adam.sotona@oracle.com>
2025-03-27 08:48:54 +0100

    WalkTheMazeTest

--- cr-examples/onnx/src/test/java/oracle/code/onnx/WalkTheMazeTest.java
+++ cr-examples/onnx/src/test/java/oracle/code/onnx/WalkTheMazeTest.java
@@ -1,8 +1,8 @@
 package oracle.code.onnx;
 
-import java.lang.foreign.MemoryLayout;
 import java.lang.foreign.ValueLayout;
 import java.util.List;
+import java.util.Optional;
 import jdk.incubator.code.CodeReflection;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -16,46 +16,50 @@ public class WalkTheMazeTest {
     static final Tensor<Byte> MAZE = Tensor.ofShape(new long[]{22, 48},
             """
             ###############################################
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
-            #                                             #
+            #     #     #   #     #     #       #   #     #
+            # # # ##### # ### ##### ##### ##### ##### # ###
+            # # #       #   #     #     # #     #         #
+            # # ######### ### ### # ### # # ##### ### #####
+            #   #         # # #       #   #       #       #
+            # ########### # # # # ####### ### ### ### ##  #
+            #   #     #   #     #       #     #     #     #
+            ### ### # # ### ############### # ##### #######
+            #       # #   #       #         # #        #  #
+            # ####### ### ##### # # ##### ##### ########  #
+            #         #   #     # # # #                   #
+            ######### ##### ##### ### # ####### ########  #
+            # #           #     #     # #     #   #       #
+            # # ######### # # ### ### # # ##### # ######  #
+            # #       #   # #   #   #       #   #         #
+            # ##### # # ##### ### ########### ### #########
+            #     # # #   #     #   #           #     #   #
+            #     # # #   #     #   #           #     #   #
+            ### # # # ### ### ##### # ####### ### ### #   #
+            #   #       #     #     #       #         #   #
             ###############################################
             """.getBytes());
 
-    static final Tensor<Long> DIRECTION_NORTH = Tensor.ofFlat('N');
-    static final Tensor<Long> DIRECTION_SOUTH = Tensor.ofFlat('S');
-    static final Tensor<Long> DIRECTION_EAST = Tensor.ofFlat('E');
-    static final Tensor<Long> DIRECTION_WEST = Tensor.ofFlat('W');
+    static final Tensor<Long> HOME_POS = Tensor.ofFlat(20l, 1); // bottom left corner
 
-    static final Tensor<Long> HOME_POS = Tensor.ofFlat(20l, 1);
+    static final String EXPECTED_PATH =
+            ">>^^>>vv>>>>>><<^^^^^<<<<<<^^>>>>>>>>>>vv<<vvv>>vv>>>>^^>>^<<^^>><<^^>><<<<^^>>>>^^>>vvvvvvvvv>>vv<<<<>>>>^^^<<^^>><<^^>>>>vv>>>>>><<^^>>>><<<<vv<"
+           +"<^^^^>>>>>>>>vvvv<<vv<<<<<<<<vvv>>>>>><<<<<<^^>>>>>>>>vv>>>>>>>>^^^<<<<^^>>>>>>>>^^^^^^<vv<<<<<<<<<^^>>>>>>><<<^^<<^^^^>>>>vv<<>>vv>>>>^^<<<<^^>>>"
+           +"><<^^>><<<<vv<<<<vv<<<<<<^^>>>>^^<<<<<<vvvv<<^^<<<<^^>>>><<<<vvvv<<^^<<<<^^>>>><<<<vvvvvv<<^^vvvv>>>>vv<<<<vvvv^^>>vvvv<<v>>vv<<<<^^^<<^^>>^^<<<<^"
+           +"^^^^^<<<<>>vv<<<<^^<<^^>>^^^^>>vv>>>>>>^^<<<<>>>>vv<<<<<<^^<<<<vvvvvv>>vv<<vv>>>>>>>>vv<<<<<<vv>>>>vvvvv<<^^^<<<<^^^^vvvvv>>vv<<>";
 
+    static final Tensor<Long> DIRECTION_NORTH = Tensor.ofFlat('^');
+    static final Tensor<Long> DIRECTION_SOUTH = Tensor.ofFlat('v');
+    static final Tensor<Long> DIRECTION_EAST = Tensor.ofFlat('>');
+    static final Tensor<Long> DIRECTION_WEST = Tensor.ofFlat('<');
     static final Tensor<Boolean> TRUE = Tensor.ofScalar(true);
-    static final Tensor<Boolean> FALSE = Tensor.ofScalar(false);
     static final Tensor<Long> ONE_ONE = Tensor.ofFlat(1l, 1);
     static final Tensor<Long> ZERO = Tensor.ofFlat(0l);
     static final Tensor<Long> TWO = Tensor.ofFlat(2l);
     static final Tensor<Long> THREE = Tensor.ofFlat(3l);
     static final Tensor<Long> MONE = Tensor.ofFlat(-1l);
-    static final Tensor<Long> MTWO = Tensor.ofFlat(-2l);
     static final Tensor<Long> MTHREE = Tensor.ofFlat(-3l);
     static final Tensor<Long> MAX = Tensor.ofFlat(Long.MAX_VALUE);
+    static final Tensor<Long> LIMIT = Tensor.ofFlat(1000l);
     static final Tensor<Long> STEP_SOUTH = Tensor.ofFlat(1l, 0);
     static final Tensor<Long> STEP_NORTH = Tensor.ofFlat(-1l, 0);
     static final Tensor<Long> STEP_EAST = Tensor.ofFlat(0l, 1);
@@ -96,70 +100,60 @@ public class WalkTheMazeTest {
                             () -> Add(myPos, STEP_SOUTH))));
     }
 
-
-    @CodeReflection
-    public static Tensor<Long> turnLeftWhileWall(Tensor<Long> pos, Tensor<Long> direction) {
-        var initialCond = Reshape(isWallAt(posInFrontOfMe(pos, direction)), SCALAR_SHAPE, empty());
-        return Loop(MAX, initialCond, direction, (_, _, newDirection) -> {
-                return LoopReturn(isWallAt(posInFrontOfMe(pos, newDirection)), turnLeft(newDirection));
-            });
-    }
-
-
     @CodeReflection
     public static Tensor<Boolean> atHome(Tensor<Long> pos) {
         return ReduceMin(Equal(pos, HOME_POS), empty(), empty(), empty());
     }
 
     @CodeReflection
-    public static Tensor<Long> walkAroundTheMaze() {
-        var start = Concat(List.of(HOME_POS, DIRECTION_EAST), 0);
-        return Loop(MAX, TRUE, start, (_, _, path) -> {
-            var pos = Slice(path, MTHREE, MONE, empty(), empty());
-            var direction = Slice(path, MONE, MAX, empty(), empty());
-
-            var posInFront = posInFrontOfMe(pos, direction);
-
-            var newPos = If(isWallAt(posInFront),
-                    () -> Identity(pos),
-                    () -> Identity(posInFront));
-
-            var newDirection =  If(isWallAt(posInFront),
-                    () -> turnLeft(direction),
-                    () -> Identity(direction));
-            return LoopReturn(Not(atHome(newPos)), Concat(List.of(path, newPos, newDirection), 0));
-        });
+    public static Tensor<Long> lastPos(Tensor<Long> pathLog) {
+        return Slice(pathLog, MTHREE, MONE, empty(), empty());
     }
 
-//    @CodeReflection
-//    public static Tensor<Long> walkAroundTheMaze2() {
-//        var start = Concat(List.of(HOME_POS, DIRECTION_EAST), 0);
-//        return Loop(MAX, TRUE, start, (_, _, path) -> {
-//            var pos = Slice(path, MTHREE, MONE, empty(), empty());
-//            var direction = Slice(path, MONE, MAX, empty(), empty());
-//            pos = posInFrontOfMe(pos, direction);
-//            direction = turnRight(direction);
-//            direction = turnLeftWhileWall(pos, direction);
-//            return LoopReturn(Not(atHome(pos)), Concat(List.of(path, pos, direction), 0));
-//        });
-//    }
+    @CodeReflection
+    public static Tensor<Long> lastDirection(Tensor<Long> pathLog) {
+        return Slice(pathLog, MONE, MAX, empty(), empty());
+    }
+
+    @CodeReflection
+    public static Tensor<Long> addToLog(Tensor<Long> pathLog, Tensor<Long> pos, Tensor<Long> direction) {
+        return Concat(List.of(pathLog, pos, direction), 0);
+    }
+
+    @CodeReflection
+    public static Tensor<Byte> extractDirections(Tensor<Long> pathLog) {
+        return Cast(Slice(pathLog, TWO, MAX, Optional.of(ZERO), Optional.of(THREE)), empty(), 3);
+    }
+
+    @CodeReflection
+    public static Tensor<Long> turnLeftWhileWall(Tensor<Long> pos, Tensor<Long> direction) {
+        var initialCond = Reshape(isWallAt(posInFrontOfMe(pos, direction)), SCALAR_SHAPE, empty());
+        return Loop(LIMIT, initialCond, direction, (_, _, dir) -> {
+                dir = turnLeft(dir);
+                return LoopReturn(isWallAt(posInFrontOfMe(pos, dir)), dir);
+            });
+    }
+
+    @CodeReflection
+    public static Tensor<Long> walkAroundTheMaze() {
+        var start = Concat(List.of(HOME_POS, DIRECTION_EAST), 0);
+        var pathLog = Loop(LIMIT, TRUE, start, (_, _, log) -> {
+            var pos = lastPos(log);
+            var direction = lastDirection(log);
+
+            // walk along the right wall
+            pos = posInFrontOfMe(pos, direction);
+            direction = turnRight(direction);
+            direction = turnLeftWhileWall(pos, direction);
+
+            return LoopReturn(Not(atHome(pos)), addToLog(log, pos, direction));
+        });
+        return pathLog;
+    }
 
     @Test
     public void testWalkAroundTheMaze() throws Exception {
-//        for (long y = 0; y < 22; y++) {
-//            for (long x = 0; x < 47; x++) {
-//                var pos = Tensor.ofFlat(y, x);
-//                System.out.print(execute(() -> isWallAt(pos)).data().get(ValueLayout.JAVA_BOOLEAN, 0) ? "#" : " ");
-//            }
-//            System.out.println();
-//        }
-
-//        var dir = execute(() -> turnLeftWhileWall(HOME_POS, DIRECTION_WEST));
-//        System.out.println((char)dir.data().get(ValueLayout.JAVA_LONG, 0));
-
-        var path = execute(() -> walkAroundTheMaze());
-        path.data().elements(MemoryLayout.sequenceLayout(3, ValueLayout.JAVA_LONG)).forEach(ms ->
-                System.out.println("x: " + ms.getAtIndex(ValueLayout.JAVA_LONG, 1) + " y: " + ms.getAtIndex(ValueLayout.JAVA_LONG, 0) + " direction: " + (char)ms.getAtIndex(ValueLayout.JAVA_LONG, 2)));
+        var directions = execute(() -> extractDirections(walkAroundTheMaze()));
+        Assertions.assertEquals(EXPECTED_PATH, new String(directions.data().toArray(ValueLayout.JAVA_BYTE)));
     }
-
 }
