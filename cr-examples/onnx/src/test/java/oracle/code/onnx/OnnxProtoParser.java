adf04d46fda1157ead39e721113ded69ff574d3b
Adam Sotona <adam.sotona@oracle.com>
2025-04-16 12:43:23 +0200

    draft of OnnxProtoParser

--- cr-examples/onnx/src/test/java/oracle/code/onnx/OnnxProtoParser.java
+++ cr-examples/onnx/src/test/java/oracle/code/onnx/OnnxProtoParser.java
@@ -14,40 +14,181 @@ import static oracle.code.onnx.OnnxProtoPrinter.decodeVarint;
 
 public final class OnnxProtoParser {
 
-    public record Attribute(String name, Float f, Long i, byte[] s, TensorProto t, GraphProto g, List<float[]> floats, List<long[]> ints, List<byte[]> strings,
-        List<TensorProto> tensors, List<GraphProto> graphs, String doc_string, TypeProto tp, List<TypeProto> type_protos, Long type, String ref_attr_name,
-        SparseTensorProto sparse_tensor, List<SparseTensorProto> sparse_tensors) {}
-    public record ValueInfoProto(String name, TypeProto type, String doc_string, List<StringStringEntryProto> metadata_props) {}
-    public record NodeProto(List<String> input, List<String> output, String name, String op_type, List<Attribute> attribute, String doc_string, String domain,
-        String overload, List<StringStringEntryProto> metadata_props) {}
-    public record TrainingInfoProto(GraphProto initialization, GraphProto algorithm, List<StringStringEntryProto> initialization_binding,
-        List<StringStringEntryProto> update_binding) {}
-    public record ModelProto(Long ir_version, String producer_name, String producer_version, String domain, Long model_version, String doc_string,
-        GraphProto graph, List<OperatorSetIdProto> opset_import, List<StringStringEntryProto> metadata_props, List<TrainingInfoProto> training_info,
-        List<FunctionProto> functions) {}
-    public record StringStringEntryProto(String key, String value) {}
-    public record TensorAnnotation(String tensor_name, List<StringStringEntryProto> quant_parameter_tensor_names) {}
-    public record GraphProto(List<NodeProto> node, String name, List<TensorProto> initializer, String doc_string, List<ValueInfoProto> input,
-        List<ValueInfoProto> output, List<ValueInfoProto> value_info, List<TensorAnnotation> quantization_annotation,
-        List<SparseTensorProto> sparse_initializer, List<StringStringEntryProto> metadata_props) {}
-    public record TensorProto(List<long[]> dims, Long data_type, Segment segment, List<float[]> float_data, List<long[]> int32_data, List<byte[]> string_data,
-        List<long[]> int64_data, String name, byte[] raw_data, List<double[]> double_data, List<long[]> uint64_data, String doc_string,
-        List<StringStringEntryProto> external_data, Long data_location, List<StringStringEntryProto> metadata_props) {}
-    public record Segment(Long begin, Long end) {}
-    public record SparseTensorProto(TensorProto values, TensorProto indices, List<long[]> dims) {}
-    public record TensorShapeProto(List<Dimension> dim) {}
-    public record Dimension(Long dim_value, String dim_param, String denotation) {}
-    public record TypeProto(Tensor tensor_type, Sequence sequence_type, Map map_type, String denotation, SparseTensor sparse_tensor_type,
-        Optional optional_type) {}
-    public record Tensor(Long elem_type, TensorShapeProto shape) {}
-    public record Sequence(TypeProto elem_type) {}
-    public record Map(Long key_type, TypeProto value_type) {}
-    public record Optional(TypeProto elem_type) {}
-    public record SparseTensor(Long elem_type, TensorShapeProto shape) {}
-    public record OperatorSetIdProto(String domain, Long version) {}
-    public record FunctionProto(String name, List<String> input, List<String> output, List<String> attribute, List<NodeProto> node, String doc_string,
-        List<OperatorSetIdProto> opset_import, String domain, List<Attribute> attribute_proto, List<ValueInfoProto> value_info, String overload,
-        List<StringStringEntryProto> metadata_props) {}
+    public record Attribute(
+            Void _0,
+            String name,
+            Float f,
+            Long i,
+            byte[] s,
+            TensorProto t,
+            GraphProto g,
+            List<float[]> floats,
+            List<long[]> ints,
+            List<byte[]> strings,
+            List<TensorProto> tensors,
+            List<GraphProto> graphs,
+            String doc_string,
+            TypeProto tp,
+            List<TypeProto> type_protos,
+            Long type,
+            String ref_attr_name,
+            SparseTensorProto sparse_tensor,
+            List<SparseTensorProto> sparse_tensors) {
+    }
+
+    public record ValueInfoProto(
+            String name,
+            TypeProto type,
+            String doc_string,
+            List<StringStringEntryProto> metadata_props) {
+    }
+
+    public record NodeProto(
+            List<String> input,
+            List<String> output,
+            String name,
+            String op_type,
+            List<Attribute> attribute,
+            String doc_string,
+            String domain,
+            String overload,
+            List<StringStringEntryProto> metadata_props) {
+    }
+
+    public record TrainingInfoProto(
+            GraphProto initialization,
+            GraphProto algorithm,
+            List<StringStringEntryProto> initialization_binding,
+            List<StringStringEntryProto> update_binding) {
+    }
+
+    public record ModelProto(
+            Long ir_version,
+            String producer_name,
+            String producer_version,
+            String domain,
+            Long model_version,
+            String doc_string,
+            GraphProto graph,
+            List<OperatorSetIdProto> opset_import,
+            List<StringStringEntryProto> metadata_props,
+            List<TrainingInfoProto> training_info,
+            List<FunctionProto> functions) {
+    }
+
+    public record StringStringEntryProto(
+            String key,
+            String value) {
+    }
+
+    public record TensorAnnotation(
+            String tensor_name,
+            List<StringStringEntryProto> quant_parameter_tensor_names) {
+    }
+
+    public record GraphProto(
+            List<NodeProto> node,
+            String name,
+            List<TensorProto> initializer,
+            String doc_string,
+            List<ValueInfoProto> input,
+            List<ValueInfoProto> output,
+            List<ValueInfoProto> value_info,
+            List<TensorAnnotation> quantization_annotation,
+            List<SparseTensorProto> sparse_initializer,
+            List<StringStringEntryProto> metadata_props) {
+    }
+
+    public record TensorProto(
+            List<long[]> dims,
+            Long data_type,
+            Segment segment,
+            List<float[]> float_data,
+            List<long[]> int32_data,
+            List<byte[]> string_data,
+            List<long[]> int64_data,
+            String name,
+            byte[] raw_data,
+            List<double[]> double_data,
+            List<long[]> uint64_data,
+            String doc_string,
+            List<StringStringEntryProto> external_data,
+            Long data_location,
+            List<StringStringEntryProto> metadata_props) {
+    }
+
+    public record Segment(
+            Long begin,
+            Long end) {
+    }
+
+    public record SparseTensorProto(
+            TensorProto values,
+            TensorProto indices,
+            List<long[]> dims) {
+    }
+
+    public record TensorShapeProto(
+            List<Dimension> dim) {
+    }
+
+    public record Dimension(
+            Long dim_value,
+            String dim_param,
+            String denotation) {
+    }
+
+    public record TypeProto(
+            Tensor tensor_type,
+            Sequence sequence_type,
+            Map map_type,
+            String denotation,
+            SparseTensor sparse_tensor_type,
+            Optional optional_type) {
+    }
+
+    public record Tensor(
+            Long elem_type,
+            TensorShapeProto shape) {
+    }
+
+    public record Sequence(
+            TypeProto elem_type) {
+    }
+
+    public record Map(
+            Long key_type,
+            TypeProto value_type) {
+    }
+
+    public record Optional(
+            TypeProto elem_type) {
+    }
+
+    public record SparseTensor(
+            Long elem_type,
+            TensorShapeProto shape) {
+    }
+
+    public record OperatorSetIdProto(
+            String domain,
+            Long version) {
+    }
+
+    public record FunctionProto(
+            String name,
+            List<String> input,
+            List<String> output,
+            List<String> attribute,
+            List<NodeProto> node,
+            String doc_string,
+            List<OperatorSetIdProto> opset_import,
+            String domain,
+            List<Attribute> attribute_proto,
+            List<ValueInfoProto> value_info,
+            String overload,
+            List<StringStringEntryProto> metadata_props) {
+    }
 
     private static long[] readPackedLongs(ByteBuffer data) {
         var ret = new long[(int)(decodeVarint(data)/4)];
