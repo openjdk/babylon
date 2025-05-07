31ac31ad9f8f77787e4105687dad4d9d3f188ead
Mourad Abbay <mourad.abbay@oracle.com>
2025-05-07 19:03:43 +0100

    Test lambda model uniqueness in parallel execution

--- test/langtools/tools/javac/reflect/LambdaModelUniquenessTest.java
+++ test/langtools/tools/javac/reflect/LambdaModelUniquenessTest.java
@@ -4,8 +4,9 @@ import jdk.incubator.code.Quoted;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.function.Function;
+import java.util.List;
 import java.util.function.IntUnaryOperator;
+import java.util.stream.Stream;
 
 /*
  * @test
@@ -21,12 +22,20 @@ public class LambdaModelUniquenessTest {
     }
 
     @Test
-    void testWithLambdaThatDoesNotCapture() {
+    void testWithLambdaThatDoNotCapture() {
         Quotable q1 = f();
         Quotable q2 = f();
         Quoted quoted1 = Op.ofQuotable(q1).orElseThrow();
         Quoted quoted2 = Op.ofQuotable(q2).orElseThrow();
         Assert.assertSame(quoted1.op(), quoted2.op());
+    }
+
+    @Test
+    void testWithLambdaThatDoNotCapture2() {
+        Quotable q1 = f();
+        Quotable q2 = f();
+        List<Op> ops = Stream.of(q1, q2).parallel().map(q -> Op.ofQuotable(q).orElseThrow().op()).toList();
+        Assert.assertSame(ops.getFirst(), ops.getLast());
     }
 
     static Quotable g(int i) {
@@ -40,5 +49,13 @@ public class LambdaModelUniquenessTest {
         Quoted quoted1 = Op.ofQuotable(q1).orElseThrow();
         Quoted quoted2 = Op.ofQuotable(q2).orElseThrow();
         Assert.assertSame(quoted1.op(), quoted2.op());
+    }
+
+    @Test
+    void testWithLambdaThatCapture2() {
+        Quotable q1 = g(1);
+        Quotable q2 = g(2);
+        List<Op> ops = Stream.of(q1, q2).parallel().map(q -> Op.ofQuotable(q).orElseThrow().op()).toList();
+        Assert.assertSame(ops.getFirst(), ops.getLast());
     }
 }
