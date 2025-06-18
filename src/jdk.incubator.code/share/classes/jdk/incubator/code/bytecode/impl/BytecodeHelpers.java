711a25570918260334e2b8d85e112585817d4c6e
Paul Sandoz <psandoz@openjdk.org>
2025-06-18 16:43:51 +0000

    Move certain classes into non-exportable packages

--- src/jdk.incubator.code/share/classes/jdk/incubator/code/bytecode/impl/BytecodeHelpers.java
+++ src/jdk.incubator.code/share/classes/jdk/incubator/code/bytecode/impl/BytecodeHelpers.java
@@ -1,13 +1,13 @@
-package jdk.incubator.code.bytecode;
+package jdk.incubator.code.bytecode.impl;
 
 import java.lang.classfile.Opcode;
 
-final class BytecodeHelpers {
+public final class BytecodeHelpers {
 
     // Copied from java.base/jdk.internal.classfile.impl.BytecodeHelpers
     // to avoid export of package from java.base to jdk.incubator.code
 
-    static boolean isUnconditionalBranch(Opcode opcode) {
+    public static boolean isUnconditionalBranch(Opcode opcode) {
         return switch (opcode) {
             case GOTO, ATHROW, GOTO_W, LOOKUPSWITCH, TABLESWITCH -> true;
             default -> opcode.kind() == Opcode.Kind.RETURN;
