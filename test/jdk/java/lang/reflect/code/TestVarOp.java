15426d4abd4ff0b0b5f63ea3f68bbdd54612fc1a
Paul Sandoz <paul.d.sandoz@googlemail.com>
2025-06-25 14:27:13 -0700

    Cleanup

--- test/jdk/java/lang/reflect/code/TestVarOp.java
+++ test/jdk/java/lang/reflect/code/TestVarOp.java
@@ -72,7 +72,6 @@ public class TestVarOp {
     @Test
     public void testNoName() {
         CoreOp.FuncOp f = getFuncOp("f");
-        System.out.println(f.toText());
         f = f.transform((block, op) -> {
             if (op instanceof CoreOp.VarOp vop) {
                 Value init = block.context().getValue(vop.initOperand());
@@ -83,7 +82,6 @@ public class TestVarOp {
             }
             return block;
         });
-        System.out.println(f.toText());
 
         Op op = OpParser.fromString(JavaOp.JAVA_DIALECT_FACTORY, f.toText()).get(0);
         boolean allNullNames = op.elements()
