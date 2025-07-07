5ebf9503c21ecfaf7d97dd5c78d2cd04372cce80
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2025-07-07 15:51:40 +0100

    Initial push

---
+++ test/langtools/tools/javac/reflect/TestNoCodeReflectionInInnerClasses.java
@@ -0,0 +1,26 @@
+/*
+ * @test /nodynamiccopyright/
+ * @modules jdk.incubator.code
+ * @compile/fail/ref=TestNoCodeReflectionInInnerClasses.out -XDrawDiagnostics TestNoCodeReflectionInInnerClasses.java
+ */
+
+import jdk.incubator.code.*;
+
+class TestNoCodeReflectionInInnerClasses {
+    class Inner {
+        @CodeReflection
+        public void test1() { }
+
+        void test2() {
+            Quotable q = (Runnable & Quotable) () -> { };
+        }
+
+        void test3() {
+            Quoted q = () -> null;
+        }
+
+        void test4() {
+            Quotable q = (Runnable & Quotable) this::test2;
+        }
+    }
+}
