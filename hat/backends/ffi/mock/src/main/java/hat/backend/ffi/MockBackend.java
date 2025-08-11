07b1939532d4077c17d5a87fa3ea01e1bf3cb6fb
Ruby Chen <ruby.r.chen@gmail.com>
2025-08-11 12:14:59 -0700

    Fix naming for ModuleOp stdout logs

--- hat/backends/ffi/mock/src/main/java/hat/backend/ffi/MockBackend.java
+++ hat/backends/ffi/mock/src/main/java/hat/backend/ffi/MockBackend.java
@@ -60,12 +60,12 @@ public class MockBackend extends FFIBackend {
         // The first time we see this we need to convert the kernel entrypoint
         // and rechable methods to a form that our mock backend can execute.
         if (Boolean.getBoolean("moduleOp")) {
-            System.out.println("Using ModuleOp for CudaBackend");
+            System.out.println("Using ModuleOp for MockBackend");
             kernelCallGraph.moduleOpWrapper.functionTable().forEach((_, funcOp) -> {
 
             });
         } else {
-            System.out.println("NOT using ModuleOp for CudaBackend");
+            System.out.println("NOT using ModuleOp for MockBackend");
             kernelCallGraph.kernelReachableResolvedStream().forEach(kr -> {
 
             });
