0e0082fdf6931f354fd28ec5d83ce1a909470e5d
Juan Fumero <jfumero@openjdk.org>
2025-09-10 13:22:53 +0000

    Initial Testing Framework for HAT

---
+++ hat/tests/src/main/java/oracle/code/hat/TestReductions.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package oracle.code.hat;
+
+import hat.Accelerator;
+import hat.ComputeContext;
+import hat.ComputeRange;
+import hat.GlobalMesh1D;
+import hat.KernelContext;
+import hat.LocalMesh1D;
+import hat.backend.Backend;
+import hat.buffer.Buffer;
+import hat.buffer.S32Array;
+import hat.ifacemapper.MappableIface;
+import hat.ifacemapper.Schema;
+import jdk.incubator.code.CodeReflection;
+import oracle.code.hat.annotation.HatTest;
+import oracle.code.hat.engine.HatAsserts;
+
+import java.lang.invoke.MethodHandles;
+
+public class TestReductions {
+
+    private interface MySharedArray extends Buffer {
+        void array(long index, int value);
+        int array(long index);
+
+        Schema<MySharedArray> schema = Schema.of(MySharedArray.class,
+                myPrivateArray -> myPrivateArray
+                        .array("array", 16));
+
+        static MySharedArray create(Accelerator accelerator) {
+            return schema.allocate(accelerator, 1);
+        }
+
+        static MySharedArray createLocal() {
+            return create(new Accelerator(MethodHandles.lookup(), Backend.FIRST));
+        }
+    }
+
+    /**
+     * Example of a simple reduction using accelerator's global memory. This is inefficient, but it shows
+     * the constructs needed to support this case, such as accessing to local ids, sizes and blocks.
+     *
+     * @param context
+     * @param input
+     * @param partialSums
+     */
+    @CodeReflection
+    private static void reduceGlobal(@MappableIface.RO KernelContext context, @MappableIface.RW S32Array input, @MappableIface.RW S32Array partialSums) {
+        int localId = context.lix;
+        int localSize = context.lsx;
+        int blockId = context.bix;
+        int baseIndex = localSize * blockId + localId;
+
+        for (int offset = localSize / 2; offset > 0; offset /= 2) {
+            if (localId < offset) {
+                int val = input.array(baseIndex);
+                val += input.array((baseIndex + offset));
+                input.array(baseIndex, val);
+            }
+            context.barrier();
+        }
+        if (localId == 0) {
+            // copy from shared memory to global memory
+            partialSums.array(blockId,  input.array(baseIndex));
+        }
+    }
+
+    /**
+     * Example of a simple parallel reduction using accelerator's local memory. This shows a proposal of how
+     * HAT could start offering local(shared) types.
+     *
+     * @param context
+     * @param input
+     * @param partialSums
+     */
+    @CodeReflection
+    private static void reduceLocal(@MappableIface.RO KernelContext context, @MappableIface.RW S32Array input, @MappableIface.RW S32Array partialSums) {
+        int localId = context.lix;
+        int localSize = context.lsx;
+        int blockId = context.bix;
+
+        // Prototype: allocate in shared memory an array of 16 ints
+        MySharedArray sharedArray = MySharedArray.createLocal();
+
+        // Copy from global to shared memory
+        sharedArray.array(localId, input.array(context.gix));
+
+        // Reduction using local memory
+        for (int offset = localSize / 2; offset > 0; offset /= 2) {
+            context.barrier();
+            if (localId < offset) {
+                sharedArray.array(localId,  sharedArray.array(localId) +  sharedArray.array(localId + offset));
+            }
+        }
+        if (localId == 0) {
+            // copy from shared memory to global memory
+            partialSums.array(blockId,  sharedArray.array(0));
+        }
+    }
+
+    private static final int BLOCK_SIZE = 16;
+
+    @CodeReflection
+    private static void reduceGlobal(@MappableIface.RO ComputeContext cc, @MappableIface.RW S32Array input, @MappableIface.RW S32Array partialSums) {
+        // 2 groups of 16 threads each
+        ComputeRange computeRange = new ComputeRange(new GlobalMesh1D(32), new LocalMesh1D(16));
+        cc.dispatchKernel(computeRange, kc -> reduceGlobal(kc, input, partialSums));
+    }
+
+    @CodeReflection
+    private static void reduceLocal(@MappableIface.RO ComputeContext cc, @MappableIface.RW S32Array input, @MappableIface.RW S32Array partialSums) {
+        // 2 groups of 16 threads each
+        ComputeRange computeRange = new ComputeRange(new GlobalMesh1D(32), new LocalMesh1D(16));
+        cc.dispatchKernel(computeRange, kc -> reduceLocal(kc, input, partialSums));
+    }
+
+    @HatTest
+    public void testGlobalReduction() {
+        Accelerator accelerator = new Accelerator(MethodHandles.lookup(), Backend.FIRST);
+        final int size = 32;
+        S32Array input = S32Array.create(accelerator, size);
+        S32Array inputOrig = S32Array.create(accelerator, size);
+        S32Array partialSums = S32Array.create(accelerator, 2);
+
+        for (int i = 0; i < size; i++) {
+            input.array(i, i);
+            inputOrig.array(i, i);
+        }
+        partialSums.fill(_ -> 0);
+
+        // Compute on the accelerator
+        accelerator.compute( cc -> TestReductions.reduceGlobal(cc, input, partialSums));
+
+        int[] results = new int[2]; // 2 groups
+        int sum = 0;
+        for (int i = 0; i < BLOCK_SIZE; i++) {
+            sum += inputOrig.array(i);
+        }
+        results[0] = sum;
+        sum = 0;
+        for (int i = BLOCK_SIZE; i < inputOrig.length(); i++) {
+            sum += inputOrig.array(i);
+        }
+        results[1] = sum;
+
+        int finalResultHat = partialSums.array(0) + partialSums.array(1);
+        int finalResultSeq = results[0] + results[1];
+        HatAsserts.assertEquals(finalResultSeq, finalResultHat);
+    }
+
+    @HatTest
+    public void testLocalReduction() {
+        Accelerator accelerator = new Accelerator(MethodHandles.lookup(), Backend.FIRST);
+        final int size = 32;
+        S32Array input = S32Array.create(accelerator, size);
+        S32Array inputOrig = S32Array.create(accelerator, size);
+        S32Array partialSums = S32Array.create(accelerator, 2);
+
+        for (int i = 0; i < size; i++) {
+            input.array(i, i);
+            inputOrig.array(i, i);
+        }
+        partialSums.fill(_ -> 0);
+
+        // Compute on the accelerator
+        accelerator.compute( cc -> TestReductions.reduceLocal(cc, input, partialSums));
+
+        int[] results = new int[2]; // 2 groups
+        int sum = 0;
+        for (int i = 0; i < BLOCK_SIZE; i++) {
+            sum += inputOrig.array(i);
+        }
+        results[0] = sum;
+        sum = 0;
+        for (int i = BLOCK_SIZE; i < inputOrig.length(); i++) {
+            sum += inputOrig.array(i);
+        }
+        results[1] = sum;
+
+        int finalResultHat = partialSums.array(0) + partialSums.array(1);
+        int finalResultSeq = results[0] + results[1];
+        HatAsserts.assertEquals(finalResultSeq, finalResultHat);
+    }
+}
---
+++ hat/tests/src/main/java/oracle/code/hat/engine/HatTestEngine.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package oracle.code.hat.engine;
+
+import oracle.code.hat.annotation.HatTest;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HatTestEngine {
+
+    public static boolean DETAIL_ERROR_STACK_TRACE = false;
+
+    private static class Stats {
+        int passed = 0;
+        int failed = 0;
+        public void incrementPassed() {
+            passed++;
+        }
+        public void incrementFailed() {
+            failed++;
+        }
+
+        public int getPassed() {
+            return passed;
+        }
+        public int getFailed() {
+            return failed;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("passed: %d, failed: %d", passed, failed);
+        }
+    }
+
+    private static void testMethod(StringBuilder builder, Method method, Stats stats, Object instance) {
+        try {
+            HatTestFormatter.testing(builder, method.getName());
+            method.invoke(instance);
+            HatTestFormatter.ok(builder);
+            stats.incrementPassed();
+        } catch (HatAssertionError e) {
+                HatTestFormatter.fail(builder);
+                stats.incrementFailed();
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            if (e.getCause() instanceof HatAssertionError hatAssertionError) {
+                HatTestFormatter.failWithReason(builder, hatAssertionError.getMessage());
+            }  else {
+                HatTestFormatter.fail(builder);
+                if (DETAIL_ERROR_STACK_TRACE) {
+                    e.printStackTrace();
+                }
+            }
+            stats.incrementFailed();
+        }
+    }
+
+    private static void filterIfNeeded(String filterMethod, List<Method> methodsToTest) {
+        // Filter the methodToTest if filterMethod is enabled
+        List<Method> replaceMethods = new ArrayList<>();
+        if (filterMethod != null) {
+            for (Method declaredMethod : methodsToTest) {
+                if (declaredMethod.getName().equals(filterMethod)) {
+                    replaceMethods.add(declaredMethod);
+                }
+            }
+            methodsToTest.clear();
+            methodsToTest.addAll(replaceMethods);
+        }
+    }
+
+    public static void printStats(StringBuilder builder, Stats stats) {
+        System.out.println();
+        System.out.println(builder.toString());
+        System.out.println(stats.toString());
+        System.out.println();
+    }
+
+    public static void testClassEngine(String classNameToTest) {
+        String filterMethod = null;
+        if (classNameToTest.contains("#")) {
+            String[] split = classNameToTest.split("#");
+            classNameToTest = split[0];
+            filterMethod = split[1];
+        }
+
+        try {
+            Class<?> testClass = Class.forName(classNameToTest);
+            // Obtain all method with the desired annotation
+            List<Method> methodToTest = new ArrayList<>();
+            for (Method declaredMethod : testClass.getDeclaredMethods()) {
+                Annotation[] declaredAnnotations = declaredMethod.getDeclaredAnnotations();
+                for (Annotation declaredAnnotation : declaredAnnotations) {
+                    if (declaredAnnotation.annotationType().equals(HatTest.class)) {
+                        methodToTest.add(declaredMethod);
+                    }
+                }
+            }
+
+            if (methodToTest.isEmpty()) {
+                throw new RuntimeException("No test methods found for class " + classNameToTest);
+            }
+
+            filterIfNeeded(filterMethod, methodToTest);
+
+            Object instance = testClass.getDeclaredConstructor().newInstance();
+
+            StringBuilder builder = new StringBuilder();
+            HatTestFormatter.appendClass(builder, classNameToTest);
+            Stats stats = new Stats();
+
+            for (Method method : methodToTest) {
+                testMethod(builder, method, stats, instance);
+            }
+            printStats(builder, stats);
+
+        } catch (ClassNotFoundException | InvocationTargetException | InstantiationException | IllegalAccessException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static void main(String[] args) {
+        System.out.println(Colours.BLUE + "HAT Engine Testing Framework" + Colours.RESET);
+        // We get a set of classes from the arguments
+        if (args.length < 1) {
+            throw new RuntimeException("No test classes provided");
+        }
+        testClassEngine(args[0]);
+    }
+}
