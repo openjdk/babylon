9010644aad5ba0f1c2c6bd90d13f70b21c10a102
grfrost <frost.gary@gmail.com>
2025-09-26 13:13:14 +0100

    separated Config and FFIConfig and cleaned up FFIConfig

--- hat/backends/ffi/shared/src/main/java/hat/backend/ffi/FFIConfig.java
+++ hat/backends/ffi/shared/src/main/java/hat/backend/ffi/FFIConfig.java
@@ -25,56 +25,76 @@
 
 package hat.backend.ffi;
 
+import hat.Config;
 import hat.codebuilders.HATCodeBuilder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
-public record Config(int bits) {
-    record Bit(int index, String name) {
+public class FFIConfig implements Config {
+    private int bits;
+    static private List<Bit> bitList;
+
+    @Override
+    public int bits(){
+        return bits;
+    }
+    @Override
+    public void bits(int bits){
+        this.bits = bits;
     }
 
-    // These must sync with hat/backends/ffi/opencl/include/opencl_backend.h
+
+    FFIConfig(int bits){
+        bits(bits);
+    }
+
+    // These must sync with hat/backends/ffi/shared/include/config.h
+    // We can create the above config by running main() below...
     // Bits 0-3 select platform id 0..5
     // Bits 4-7 select device id 0..15
-    private static final int START_BIT_IDX = 16;
-    private static final int MINIMIZE_COPIES_BIT = 1 << START_BIT_IDX;
-    private static final int TRACE_BIT = 1 << 17;
-    private static final int PROFILE_BIT = 1 << 18;
-    private static final int SHOW_CODE_BIT = 1 << 19;
-    private static final int SHOW_KERNEL_MODEL_BIT = 1 << 20;
-    private static final int SHOW_COMPUTE_MODEL_BIT = 1 << 21;
-    private static final int INFO_BIT = 1 << 22;
-    private static final int TRACE_COPIES_BIT = 1 << 23;
-    private static final int TRACE_SKIPPED_COPIES_BIT = 1 << 24;
-    private static final int TRACE_ENQUEUES_BIT = 1 << 25;
-    private static final int TRACE_CALLS_BIT = 1 << 26;
-    private static final int SHOW_WHY_BIT = 1 << 27;
-    private static final int SHOW_STATE_BIT = 1 << 28;
-    private static final int PTX_BIT = 1 << 29;
-    private static final int INTERPRET_BIT = 1 << 30;
-    private static final int END_BIT_IDX = 31;
+    // Bits 8-15 unused at present.
+    // These bits start at 16
 
-    private static String[] bitNames = {
-            "MINIMIZE_COPIES",
-            "TRACE",
-            "PROFILE",
-            "SHOW_CODE",
-            "SHOW_KERNEL_MODEL",
-            "SHOW_COMPUTE_MODEL",
-            "INFO",
-            "TRACE_COPIES",
-            "TRACE_SKIPPED_COPIES",
-            "TRACE_ENQUEUES",
-            "TRACE_CALLS",
-            "SHOW_WHY",
-            "SHOW_STATE",
-            "PTX",
-            "INTERPRET",
-    };
+    public static final Bit MINIMIZE_COPIES =  Bit.of(16, "MINIMIZE_COPIES");
+    public static final Bit TRACE = Bit.nextBit(MINIMIZE_COPIES,"TRACE");
+    public static final Bit PROFILE = Bit.nextBit(TRACE, "PROFILE");
+    public static final  Bit SHOW_CODE = Bit.nextBit(PROFILE,"SHOW_CODE");
+    public static final Bit SHOW_KERNEL_MODEL = Bit.nextBit(SHOW_CODE,"SHOW_KERNEL_MODEL");
+    public static final Bit SHOW_COMPUTE_MODEL = Bit.nextBit(SHOW_KERNEL_MODEL,"SHOW_COMPUTE_MODEL");
+    public static final Bit INFO = Bit.nextBit(SHOW_COMPUTE_MODEL, "INFO");
+    public static final Bit TRACE_COPIES = Bit.nextBit(INFO, "TRACE_COPIES");
+    public static final Bit TRACE_SKIPPED_COPIES = Bit.nextBit(TRACE_COPIES, "TRACE_SKIPPED_COPIES");
+    public static final Bit TRACE_ENQUEUES = Bit.nextBit(TRACE_SKIPPED_COPIES,"TRACE_ENQUEUES");
+    public static final Bit TRACE_CALLS= Bit.nextBit(TRACE_ENQUEUES, "TRACE_CALLS");
+    public static final Bit SHOW_WHY = Bit.nextBit(TRACE_CALLS, "SHOW_WHY");
+    public static final Bit SHOW_STATE = Bit.nextBit(SHOW_WHY, "SHOW_STATE");
+    public static final Bit PTX = Bit.nextBit(SHOW_STATE, "PTX");
+    public static final Bit INTERPRET = Bit.nextBit(PTX, "INTERPRET");
 
-    public static Config of() {
+    static {
+        bitList = List.of(
+                MINIMIZE_COPIES,
+                TRACE,
+                PROFILE,
+                SHOW_CODE,
+                SHOW_KERNEL_MODEL,
+                SHOW_COMPUTE_MODEL,
+                INFO,
+                TRACE_COPIES,
+                TRACE_SKIPPED_COPIES,
+                TRACE_ENQUEUES,
+                TRACE_CALLS,
+                SHOW_WHY,
+                SHOW_STATE,
+                PTX,
+                INTERPRET
+        );
+    }
+
+    public static FFIConfig of() {
         if (System.getenv("HAT") instanceof String opts) {
             System.out.println("From env " + opts);
             return of(opts);
@@ -86,198 +106,89 @@ public record Config(int bits) {
         return of("");
     }
 
-    public static Config of(int bits) {
-        return new Config(bits);
+    public static FFIConfig of(int bits) {
+        return new FFIConfig(bits);
     }
 
-    public static Config of(List<Config> configs) {
+    public static FFIConfig of(List<Config.Bit> configBits) {
         int allBits = 0;
-        for (Config config : configs) {
-            allBits |= config.bits;
+        for (Config.Bit configBit : configBits) {
+            allBits |= configBit.shifted();
         }
-        return new Config(allBits);
+        return new FFIConfig(allBits);
     }
 
-    public static Config of(Config... configs) {
-        return of(List.of(configs));
+    public static FFIConfig of(Config.Bit... configBits) {
+        return of(List.of(configBits));
     }
 
-    public Config and(Config... configs) {
-        return Config.of(Config.of(List.of(configs)).bits & bits);
+    public FFIConfig and(Config.Bit... configBits) {
+        return FFIConfig.of(FFIConfig.of(List.of(configBits)).bits & bits);
     }
 
-    public Config or(Config... configs) {
-        return Config.of(Config.of(List.of(configs)).bits | bits);
+    public FFIConfig or(Config.Bit... configBits) {
+        return FFIConfig.of(FFIConfig.of(List.of(configBits)).bits | bits);
     }
 
-    public static Config of(String name) {
+    public static FFIConfig of(String name) {
         if (name == null || name.equals("")) {
-            return Config.of(0);
+            return FFIConfig.of(0);
         }
-        for (int i = 0; i < bitNames.length; i++) {
-            if (bitNames[i].equals(name)) {
-                return new Config(1 << (i + START_BIT_IDX));
+
+        for (Bit bit:bitList) {
+            if (bit.name().equals(name)) {
+                return new FFIConfig(bit.shifted());
             }
         }
         if (name.contains(",")) {
-            List<Config> configs = new ArrayList<>();
-            Arrays.stream(name.split(",")).forEach(opt ->
-                    configs.add(of(opt))
+            List<Config.Bit> configBits = new ArrayList<>();
+            Arrays.stream(name.split(",")).forEach(opt -> {
+                   boolean found = false;
+                   for (var bit:FFIConfig.bitList) {
+                       if (bit.name().equals(opt)) {
+                           configBits.add(bit);
+                           found = true;
+                           break;
+                       }
+                   }
+                   if (!found){
+                       throw new IllegalStateException("WHAT HAT OPT ?"+opt);
+
+                   }
+            }
             );
-            return of(configs);
+            return of(configBits);
         } else if (name.contains(":")) {
             var tokens = name.split(":");
             if (tokens.length == 2) {
                 var token = tokens[0];
                 if (token.equals("PLATFORM") || token.equals("DEVICE")) {
                     int value = Integer.parseInt(tokens[1]);
-                    return new Config(value << (token.equals("DEVICE") ? 4 : 0));
+                    return new FFIConfig(value << (token.equals("DEVICE") ? 4 : 0));
                 } else {
                     System.out.println("Unexpected opt '" + name + "'");
-                    return Config.of(0);
+                    return FFIConfig.of(0);
                 }
             } else {
                 System.out.println("Unexpected opt '" + name + "'");
-                return Config.of(0);
+                return FFIConfig.of(0);
             }
         } else {
             System.out.println("Unexpected opt '" + name + "'");
             System.exit(1);
-            return Config.of(0);
+            return FFIConfig.of(0);
         }
-    }
-
-    public static Config PTX() {
-        return new Config(PTX_BIT);
-    }
-
-    public boolean isINTERPRET() {
-        return (bits & INTERPRET_BIT) == INTERPRET_BIT;
-    }
-
-    public static Config INTERPRET() {
-        return new Config(INTERPRET_BIT);
-    }
-
-    public boolean isPTX() {
-        return (bits & PTX_BIT) == PTX_BIT;
-    }
-
-    public static Config SHOW_STATE() {
-        return new Config(SHOW_STATE_BIT);
-    }
-
-    public boolean isSHOW_STATE() {
-        return (bits & SHOW_STATE_BIT) == SHOW_STATE_BIT;
-    }
-
-    public static Config SHOW_WHY() {
-        return new Config(SHOW_WHY_BIT);
-    }
-
-    public boolean isSHOW_WHY() {
-        return (bits & SHOW_WHY_BIT) == SHOW_WHY_BIT;
-    }
-
-    public static Config TRACE_COPIES() {
-        return new Config(TRACE_COPIES_BIT);
-    }
-
-    public boolean isTRACE_COPIES() {
-        return (bits & TRACE_COPIES_BIT) == TRACE_COPIES_BIT;
-    }
-
-    public static Config TRACE_CALLS() {
-        return new Config(TRACE_CALLS_BIT);
-    }
-
-    public boolean isTRACE_CALLS() {
-        return (bits & TRACE_CALLS_BIT) == TRACE_CALLS_BIT;
-    }
-
-    public static Config TRACE_ENQUEUES() {
-        return new Config(TRACE_ENQUEUES_BIT);
-    }
-
-    public boolean isTRACE_ENQUEUES() {
-        return (bits & TRACE_ENQUEUES_BIT) == TRACE_ENQUEUES_BIT;
-    }
-
-
-    public static Config TRACE_SKIPPED_COPIES() {
-        return new Config(TRACE_SKIPPED_COPIES_BIT);
-    }
-
-    public boolean isTRACE_SKIPPED_COPIES() {
-        return (bits & TRACE_SKIPPED_COPIES_BIT) == TRACE_SKIPPED_COPIES_BIT;
-    }
-
-    public static Config INFO() {
-        return new Config(INFO_BIT);
-    }
-
-    public boolean isINFO() {
-        return (bits & INFO_BIT) == INFO_BIT;
-    }
-
-
-    public static Config PROFILE() {
-        return new Config(PROFILE_BIT);
-    }
-
-    public boolean isPROFILE() {
-        return (bits & PROFILE_BIT) == PROFILE_BIT;
-    }
-
-    public static Config TRACE() {
-        return new Config(TRACE_BIT);
-    }
-
-    public boolean isTRACE() {
-        return (bits & TRACE_BIT) == TRACE_BIT;
-    }
-
-    public static Config MINIMIZE_COPIES() {
-        return new Config(MINIMIZE_COPIES_BIT);
-    }
-
-    public boolean isMINIMIZE_COPIES() {
-        return (bits & MINIMIZE_COPIES_BIT) == MINIMIZE_COPIES_BIT;
-    }
-
-    public static Config SHOW_CODE() {
-        return new Config(SHOW_CODE_BIT);
-    }
-
-    public boolean isSHOW_CODE() {
-        return (bits & SHOW_CODE_BIT) == SHOW_CODE_BIT;
-    }
-
-    public static Config SHOW_KERNEL_MODEL() {
-        return new Config(SHOW_KERNEL_MODEL_BIT);
-    }
-
-    public boolean isSHOW_KERNEL_MODEL() {
-        return (bits & SHOW_KERNEL_MODEL_BIT) == SHOW_KERNEL_MODEL_BIT;
-    }
-
-    public static Config SHOW_COMPUTE_MODEL() {
-        return new Config(SHOW_COMPUTE_MODEL_BIT);
-    }
-
-    public boolean isSHOW_COMPUTE_MODEL() {
-        return (bits & SHOW_COMPUTE_MODEL_BIT) == SHOW_COMPUTE_MODEL_BIT;
     }
 
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        for (int bitIdx = START_BIT_IDX; bitIdx < END_BIT_IDX; bitIdx++) {
-            if ((bits & (1 << bitIdx)) == (1 << bitIdx)) {
+        for (Bit bit:bitList){
+            if (bit.isSet(bits)) {
                 if (!builder.isEmpty()) {
                     builder.append("|");
                 }
-                builder.append(bitNames[bitIdx - START_BIT_IDX]);
+                builder.append(bit.name());
 
             }
         }
@@ -346,10 +257,10 @@ public record Config(int bits) {
         }
 
         public static void main(String[] args) {
-            Config c = Config.of("INFO,PTX");
+            FFIConfig c = FFIConfig.of("INFO,PTX");
             ConfigBuilder cb = new ConfigBuilder();
 
-            cb.lineComment("Auto generated from  " + Config.class.getName());
+            cb.lineComment("Auto generated from  " + FFIConfig.class.getName());
             cb.pragma("once").nl();
             cb.includeSys("iostream").nl();
             final int START_BIT_INDEX = 0x10;
@@ -357,15 +268,15 @@ public record Config(int bits) {
             cb.structKeyword().space().className().braceNlIndented((_) -> {
                 cb.staticConstInt("START_BIT_IDX", 32, START_BIT_INDEX);
                 int i = START_BIT_INDEX;
-                for (var bitname : Config.bitNames) {
-                    cb.staticConstIntShiftedOne(bitname + "_BIT", 32, i++);
+                for (var bit : FFIConfig.bitList) {
+                    cb.staticConstIntShiftedOne(bit.name() + "_BIT", 32, i++);
                 }
                 cb.staticConstInt("END_BIT_IDX", 32, i);
                 cb.constKeyword().space().staticKeyword().space().charType().space().asterisk().bitNamesVar().osbrace().csbrace().semicolon().space().lineComment("See below for initialization");
                 cb.intType().space().identifier("configBits").semicolon().nl();
 
-                for (var bitName : Config.bitNames) {
-                    cb.identifier("bool").space().camelExceptFirst(bitName).semicolon().nl();
+                for (var bit : FFIConfig.bitList) {
+                    cb.identifier("bool").space().camelExceptFirst(bit.name()).semicolon().nl();
                 }
 
                 cb.intType().space().identifier("platform").semicolon().nl();
@@ -374,8 +285,8 @@ public record Config(int bits) {
 //Constructor
                 cb.explicitKeyword().space().className().paren((_) -> cb.intType().space().configBitsVar()).colon().nl().indent((_) -> {
                     cb.configBitsVar().paren((_) -> cb.configBitsVar()).comma().nl();
-                    for (var bitName : Config.bitNames) {
-                        cb.camelExceptFirst(bitName).paren((_) -> cb.paren((_) -> cb.configBitsAndBitName(bitName)).eq().identifier(bitName + "_BIT")).comma().nl();
+                    for (var bit : FFIConfig.bitList) {
+                        cb.camelExceptFirst(bit.name()).paren((_) -> cb.paren((_) -> cb.configBitsAndBitName(bit.name())).eq().identifier(bit.name() + "_BIT")).comma().nl();
 
                     }
                     cb.identifier("platform").paren((_) -> cb.configBitsAnd().intHexValue(0xf)).comma().nl();
@@ -383,8 +294,8 @@ public record Config(int bits) {
                     cb.identifier("device").paren(_ ->
                             cb.paren(_ -> cb.configBitsAnd().intHexValue(0xf0)).space().rightShift().space().intValue(4)).braceNlIndented(_ ->
                             cb.ifKeyword().paren(_ -> cb.identifier("info")).braceNlIndented(_ -> {
-                                for (var bitName : Config.bitNames) {
-                                    cb.stdCout("native " + ConfigBuilder.toCamelExceptFirst(bitName) + " ").space().leftShift().space().camelExceptFirst(bitName).space().leftShift().space().stdEndl().semicolon().nl();
+                                for (var bit : FFIConfig.bitList) {
+                                    cb.stdCout("native " + ConfigBuilder.toCamelExceptFirst(bit.name()) + " ").space().leftShift().space().camelExceptFirst(bit.name()).space().leftShift().space().stdEndl().semicolon().nl();
                                 }
                             })
                     );
@@ -398,8 +309,8 @@ public record Config(int bits) {
             cb.hashIfdef("shared_cpp", (_) -> {
                 cb.constKeyword().space().charType().space().asterisk().className().colon().colon().bitNamesVar().sbrace(_ -> {}).equals().brace((_) -> {
                     cb.nl();
-                    for (var bitName : Config.bitNames) {
-                        cb.dquote().identifier(bitName + "_BIT").dquote().comma().nl();
+                    for (var bit : FFIConfig.bitList) {
+                        cb.dquote().identifier(bit.name() + "_BIT").dquote().comma().nl();
                     }
                 }).semicolon().nl();
             });
---
+++ hat/core/src/main/java/hat/Config.java
@@ -0,0 +1,32 @@
+package hat;
+
+import java.util.Map;
+
+public interface Config {
+    int bits();
+    void bits(int bits);
+    record Bit(int index, int size, String name) implements Comparable<Bit> {
+        static Bit of(int index, int size, String name){
+            return new Bit(index,size,name);
+        }
+        public static Bit of(int index, String name){
+            return new Bit(index,1,name);
+        }
+        public static Bit nextBit(Bit bit, String name){
+            System.out.println(name+ " = 1<<"+ bit.index);
+            return new Bit(bit.index+1,1,name);
+        }
+
+        @Override
+        public int compareTo(Bit bit) {
+            return Integer.compare(index, bit.index);
+        }
+
+        public boolean isSet(int bits){
+            return (shifted()&bits) == shifted();
+        }
+        public int shifted(){
+            return 1<<index;
+        }
+    }
+}
