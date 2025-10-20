d2e5ba5ad6c57d11920de43c42a879b2f741ab7e
Adam Sotona <adam.sotona@oracle.com>
2025-10-20 18:33:28 +0200

    BytecodeLift and related tests move under test/jdk/java/lang/reflect/code/bytecode/lift

--- test/jdk/java/lang/reflect/code/bytecode/lift/TestLiftCustomBytecode.java
+++ test/jdk/java/lang/reflect/code/bytecode/lift/TestLiftCustomBytecode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,7 +21,6 @@
  * questions.
  */
 
-import jdk.incubator.code.bytecode.BytecodeLift;
 import jdk.incubator.code.dialect.core.CoreOp;
 import jdk.incubator.code.interpreter.Interpreter;
 import org.junit.jupiter.api.Assertions;
@@ -35,7 +34,7 @@ import java.lang.invoke.StringConcatFactory;
 
 /*
  * @test
- * @modules jdk.incubator.code
+ * @modules jdk.incubator.code/jdk.incubator.code.internal
  * @enablePreview
  * @run junit TestLiftCustomBytecode
  */
