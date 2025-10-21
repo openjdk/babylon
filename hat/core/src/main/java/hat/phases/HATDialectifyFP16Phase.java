5d8378cd960eec74f651041e39f1c27e7cce625f
Juan Fumero <jfumero@openjdk.org>
2025-10-21 09:17:59 +0200

    [hat] missing header license added

--- hat/core/src/main/java/hat/phases/HATDialectifyFP16Phase.java
+++ hat/core/src/main/java/hat/phases/HATDialectifyFP16Phase.java
@@ -1,7 +1,30 @@
+/*
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
 package hat.phases;
 
 import hat.Accelerator;
-import hat.ComputeRange;
 import hat.buffer.F16Array;
 import hat.dialect.HATF16BinaryOp;
 import hat.dialect.HATF16VarLoadOp;
@@ -15,7 +38,6 @@ import jdk.incubator.code.dialect.core.CoreOp;
 import jdk.incubator.code.dialect.java.JavaOp;
 import jdk.incubator.code.dialect.java.JavaType;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -188,7 +210,6 @@ public class HATDialectifyFP16Phase implements HATDialect {
         funcOp = dialectifyF16Stores(funcOp);
         return funcOp;
     }
-
 
     @Override
     public Accelerator accelerator() {
