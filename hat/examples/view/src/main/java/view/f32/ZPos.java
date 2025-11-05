b6136293a7ce87ff9fa585acc2825cbe620f563e
grfrost <frost.gary@gmail.com>
2025-11-05 16:50:02 +0000

    towards common pools

--- hat/examples/view/src/main/java/view/f32/ZPos.java
+++ hat/examples/view/src/main/java/view/f32/ZPos.java
@@ -40,7 +40,7 @@ public class ZPos implements Comparable<ZPos> {
         return Float.compare(z, zPos.z);
     }
 
-    public ZPos(F32Triangle3D.F32Triangle3DImpl t, float howVisible) {
+    public ZPos(F32Triangle3D t, float howVisible) {
         F32Vec3 v0 = t.v0();
         F32Vec3  v1 = t.v1();
         F32Vec3 v2 = t.v2();
