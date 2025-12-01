b9a75e2c0a925a86f496d1fa7795ab7c79992c9f
Adam Sotona <adam.sotona@oracle.com>
2025-12-01 17:57:27 +0100

    test update

--- test/jdk/java/lang/reflect/code/bytecode/TestSwitch.java
+++ test/jdk/java/lang/reflect/code/bytecode/TestSwitch.java
@@ -21,6 +21,10 @@
  * questions.
  */
 
+import java.lang.classfile.ClassFile;
+import java.lang.classfile.MethodModel;
+import java.lang.classfile.instruction.LookupSwitchInstruction;
+import java.lang.classfile.instruction.TableSwitchInstruction;
 import jdk.incubator.code.Reflect;
 import jdk.incubator.code.Op;
 import jdk.incubator.code.bytecode.BytecodeGenerator;
@@ -31,14 +35,8 @@ import org.junit.jupiter.api.Test;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Method;
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
-import jdk.incubator.code.Block;
-import jdk.incubator.code.Value;
-import jdk.incubator.code.bytecode.impl.ConstantLabelSwitchOp;
-import jdk.incubator.code.dialect.core.CoreType;
-import jdk.incubator.code.dialect.java.JavaType;
 
 /*
  * @test
@@ -58,57 +56,13 @@ public class TestSwitch {
     }
 
     @Test
-    public void switchExpression() throws Throwable {
-//        CoreOp.FuncOp f = getFuncOp("switchExpression");
+    public void testSwitchExpression() throws Throwable {
+        CoreOp.FuncOp f = getFuncOp("switchExpression");
 
-        // @@@ manually constructed:
-        // func @"switchExpression" (%0 : java.type:"int")java.type:"java.lang.String" -> {
-        //    %1 : Var<java.type:"int"> = var %0 @"i";
-        //    %2 : java.type:"int" = var.load %1;
-        //    ConstantLabelSwitchOp %2 @"[7,42,101]" ^block_1 ^block_2 ^block_3 ^block_4;
-        //
-        //  ^block_1:
-        //    %3 : java.type:"java.lang.String" = constant @"magic number";
-        //    branch ^block_5(%3);
-        //
-        //  ^block_2:
-        //    %4 : java.type:"java.lang.String" = constant @"Answer to the Ultimate Question of Life, the Universe, and Everything";
-        //    branch ^block_5(%4);
-        //
-        //  ^block_3:
-        //    %5 : java.type:"java.lang.String" = constant @"introduction to a subject";
-        //    branch ^block_5(%5);
-        //
-        //  ^block_4:
-        //    %6 : java.type:"java.lang.String" = constant @"not important";
-        //    branch ^block_5(%6);
-        //
-        //  ^block_5(%7 : java.type:"java.lang.String"):
-        //    return %7;
-        //};
-        JavaType stringType = JavaType.type(String.class);
-        CoreOp.FuncOp f = CoreOp.FuncOp.func("switchExpression",
-                CoreType.functionType(stringType, JavaType.INT)).body(bb -> {
-                    Block.Builder b0 = bb.entryBlock(),
-                                  b1 = bb.block(),
-                                  b2 = bb.block(),
-                                  b3 = bb.block(),
-                                  b4 = bb.block(),
-                                  b5 = bb.block(stringType);
-                    Value v1 = b0.op(CoreOp.var("i", JavaType.INT, b0.parameters().getFirst()));
-                    Value v2 = b0.op(CoreOp.varLoad(v1));
-                    b0.op(new ConstantLabelSwitchOp(v2,
-                            List.of(7, 42, 101),
-                            List.of(b1.successor(), b2.successor(), b3.successor(), b4.successor())));
-                    b1.op(CoreOp.branch(b5.successor(b1.op(CoreOp.constant(stringType, "magic number")))));
-                    b2.op(CoreOp.branch(b5.successor(b2.op(CoreOp.constant(stringType, "Answer to the Ultimate Question of Life, the Universe, and Everything")))));
-                    b3.op(CoreOp.branch(b5.successor(b3.op(CoreOp.constant(stringType, "introduction to a subject")))));
-                    b4.op(CoreOp.branch(b5.successor(b4.op(CoreOp.constant(stringType, "not important")))));
-                    b5.op(CoreOp.return_(b5.parameters().getFirst()));
-                });
+        Assertions.assertTrue(getModel(f).code().get().elementStream()
+                .anyMatch(i -> i instanceof TableSwitchInstruction || i instanceof LookupSwitchInstruction));
 
         MethodHandle mh = generate(f);
-
         for (int i = 0; i < 110; i++) {
             Assertions.assertEquals(switchExpression(i), (String)mh.invokeExact(i));
         }
@@ -120,8 +74,15 @@ public class TestSwitch {
         return BytecodeGenerator.generate(MethodHandles.lookup(), f);
     }
 
+    static MethodModel getModel(CoreOp.FuncOp f) {
+        var clm = ClassFile.of().parse(BytecodeGenerator.generateClassData(MethodHandles.lookup(), f));
+        var mm = clm.methods().getFirst();
+        System.out.println(mm.toDebugString());
+        return mm;
+    }
+
     static CoreOp.FuncOp getFuncOp(String name) {
-        Optional<Method> om = Stream.of(TestTry.class.getDeclaredMethods())
+        Optional<Method> om = Stream.of(TestSwitch.class.getDeclaredMethods())
                 .filter(m -> m.getName().equals(name))
                 .findFirst();
 
