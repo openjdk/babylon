336899182b4edc2d686b88070758dfa9f02c6e78
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2025-12-12 11:24:54 +0000

    Initial push

--- src/jdk.incubator.code/share/classes/jdk/incubator/code/internal/ReflectMethods.java
+++ src/jdk.incubator.code/share/classes/jdk/incubator/code/internal/ReflectMethods.java
@@ -107,6 +107,7 @@ import static com.sun.tools.javac.code.Flags.*;
 import static com.sun.tools.javac.code.Kinds.Kind.MTH;
 import static com.sun.tools.javac.code.Kinds.Kind.TYP;
 import static com.sun.tools.javac.code.Kinds.Kind.VAR;
+import static com.sun.tools.javac.code.TypeTag.ARRAY;
 import static com.sun.tools.javac.code.TypeTag.BOT;
 import static com.sun.tools.javac.code.TypeTag.CLASS;
 import static com.sun.tools.javac.code.TypeTag.METHOD;
@@ -1864,14 +1865,16 @@ public class ReflectMethods extends TreeTranslatorPrev {
             popBody();
 
             JCVariableDecl var = tree.getVariable();
-            JavaType eType = typeToTypeElement(var.type);
             VarType varEType = CoreType.varType(typeToTypeElement(var.type));
 
             // Push init
             // @@@ When lhs assignment is a pattern we embed the pattern match into the init body and
             // return the bound variables
-            pushBody(var, CoreType.functionType(varEType, eType));
-            Op.Result varEResult = append(CoreOp.var(var.name.toString(), stack.block.parameters().get(0)));
+            Type eType = tree.expr.type.hasTag(ARRAY) ?
+                    types.elemtype(tree.expr.type) : types.asSuper(tree.expr.type, syms.iterableType.tsym).getTypeArguments().head;
+            pushBody(var, CoreType.functionType(varEType, typeToTypeElement(eType)));
+            var initVarExpr = convert(stack.block.parameters().get(0), var.type);
+            Op.Result varEResult = append(CoreOp.var(var.name.toString(), initVarExpr));
             append(CoreOp.core_yield(varEResult));
             Body.Builder init = stack.body;
             // Pop init
