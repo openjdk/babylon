4c2a687d9e25c57e905271e76287368f0698eebf
grfrost <frost.gary@gmail.com>
2026-02-18 14:47:09 +0000

    Whitespace

--- hat/blue.java
+++ hat/blue.java
@@ -3,7 +3,7 @@ import static hat.types.vec4.*;
 
 static void main(String[] args) throws IOException {
     shade.ShaderFrame.of( 1024, 1024, 30, "Blue",
-        (uniforms, FragColor, fragCoord) -> 
-           vec4(0f,0f,5f,0f) 
+        (uniforms, FragColor, fragCoord) ->
+           vec4(0f,0f,5f,0f)
     );
 }
