1c04bdfce221d80e0760f16c43166a9cbbd301b4
grfrost <frost.gary@gmail.com>
2026-02-18 14:35:01 +0000

    updated gradient  scripted shader

--- hat/gradient.java
+++ hat/gradient.java
@@ -1,20 +1,17 @@
 import hat.backend.Backend;
 
-import javax.swing.JFrame;
-import java.awt.Rectangle;
-import hat.Accelerator;
-import hat.backend.Backend;
-import hat.types.ivec2;
-import hat.types.vec2;
 import shade.Shader;
+import hat.types.vec3;
+import hat.types.vec2;
 import static hat.types.F32.*;
 import static hat.types.vec4.*;
 import static hat.types.vec2.*;
+import static hat.types.vec3.*;
 
 
 static void main(String[] args) throws IOException {
     Shader shader = (uniforms, inFragColor, fragCoord) -> {
-            var uv = div(fragCoord,vec2(uniforms.iResolution()));  // normalize between 0->1 vec2 uv = fragCoord/iResolution.xy
+            var uv = div(fragCoord,vec3.xy(uniforms.iResolution()));  // normalize between 0->1 vec2 uv = fragCoord/iResolution.xy
             float frame= max(uniforms.iFrame()/1000f,1f);
             return vec4(uv.x(),uv.y(),frame,0f);
     };
