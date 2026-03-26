41eda810c56664c73b60dda0b2a29a1a79d76415
Adam Sotona <adam.sotona@oracle.com>
2026-03-26 23:43:50 +0100

    fix javadoc wording

--- src/java.base/share/classes/java/lang/invoke/AbstractValidatingLambdaMetafactory.java
+++ src/java.base/share/classes/java/lang/invoke/AbstractValidatingLambdaMetafactory.java
@@ -107,9 +107,9 @@ import static sun.invoke.util.Wrapper.isWrapperType;
      *                      should implement.
      * @param altMethods Method types for additional signatures to be
      *                   implemented by invoking the implementation method
-     * @param finisher A function called at the end of the lambda class build process
-     *                 that returns an additional object to append to the class data.
-     *                 It may be null and it may return null
+     * @param finisher Function called at the end of the lambda class build process
+     *                 that returns an additional object to append to the class data,
+     *                 may be (@code null}, may return {@code null}.
      * @throws LambdaConversionException If any of the meta-factory protocol
      *         invariants are violated
      */
