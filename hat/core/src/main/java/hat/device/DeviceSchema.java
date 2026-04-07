6a27635f185a60f821681840411b03d4eb7aa67d
Gary Frost <gfrost@openjdk.org>
2026-04-07 11:24:31 +0000

    Some cleanup of c99ffibackends and device schema

--- hat/core/src/main/java/hat/device/DeviceSchema.java
+++ hat/core/src/main/java/hat/device/DeviceSchema.java
@@ -24,27 +24,31 @@
  */
 package hat.device;
 
+import hat.codebuilders.C99HATKernelBuilder;
+import optkl.IfaceValue;
+
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class DeviceSchema<T extends NonMappableIface> {
-    public final Class<T> clazz;
+
     public interface Builder<T extends NonMappableIface>{
         DeviceSchema<T> deviceSchema();
         Class<?> clazz();
-        List<Builder<T>> members();
-
+        List<NamedMember<T>> members();
         default Class<?> getReturnType(String fieldName){
             return Arrays.stream(clazz().getDeclaredMethods())
                     .filter(m->m.getName().equals(fieldName) && !m.getReturnType().equals(void.class))
-                    .map(m->m.getReturnType()).findFirst().get();
+                    .map(Method::getReturnType).findFirst().orElseThrow();
         }
 
         default Builder<T> fields(String... fieldNames) {
-            var fieldNameList = List.of(fieldNames);
-            members().addAll(fieldNameList.stream().map(fieldName->new Field<>(this,getReturnType(fieldName),fieldName,new ArrayList<>())).toList());
+            Stream.of(fieldNames).map(fieldName->new Field<>(this, getReturnType(fieldName), fieldName, new ArrayList<>()))
+                    .forEach(field->members().add(field));
             return this;
         }
 
@@ -52,49 +56,65 @@ public class DeviceSchema<T extends NonMappableIface> {
             return fields(fieldName);
         }
 
-      //  default Builder<T> array(String fieldName, int size) {
-        //    members().add(new Array<>(this,getReturnType(fieldName),fieldName,size,new ArrayList<>()));
-          //  return this;
-       // }
         default Builder<T> array(String fieldName, int size) {
-            var retType = getReturnType(fieldName);
-            //if (!retType.equals(clazz)){
-              //  throw new RuntimeException("Oh my");
-            //}
-            members().add(new Array<>(this,retType,fieldName,size,new ArrayList<>()));
+            members().add(new Array<>(this, getReturnType(fieldName), fieldName,size, new ArrayList<>()));
             return this;
         }
 
         default Builder<T> array(String fieldName, int size, Consumer<Builder<T>> depConsumer) {
-            array(fieldName,size);
-            depConsumer.accept(members().getLast());
+            depConsumer.accept(array(fieldName,size).members().getLast());
             return this;
         }
     }
     public interface NamedMember<T extends NonMappableIface> extends Builder<T> {
         Builder<T> parent();
         String name();
-        @Override
-        default DeviceSchema<T> deviceSchema() {
+        @Override default DeviceSchema<T> deviceSchema() {
             return parent().deviceSchema();
         }
     }
-    public interface NamedArrayMember<T extends NonMappableIface> extends NamedMember<T>{
-        int size();
-    }
-    public record Root<T extends NonMappableIface>(DeviceSchema<T> deviceSchema, Class<T> clazz, List<Builder<T>> members)implements Builder<T>{};
-    public record Field<T extends NonMappableIface>(Builder<T> parent, Class<?> clazz, String name, List<Builder<T>> members)implements NamedMember<T> {}
-    public record Array<T extends NonMappableIface>(Builder<T> parent, Class<?> clazz, String name, int size, List<Builder<T>> members) implements NamedArrayMember<T> {}
-    public final Root<T> root;
-    public DeviceSchema(Class<T> clazz) {
-        this.clazz = clazz;
-        this.root = new Root<>(this,clazz, new ArrayList<>());
-    }
 
+    public record Root<T extends NonMappableIface>(DeviceSchema<T> deviceSchema, Class<T> clazz, List<NamedMember<T>> members)implements Builder<T>{};
+    public record Field<T extends NonMappableIface>(Builder<T> parent, Class<?> clazz, String name, List<NamedMember<T>> members)implements NamedMember<T> {}
+    public record Array<T extends NonMappableIface>(Builder<T> parent, Class<?> clazz, String name, int size, List<NamedMember<T>> members) implements NamedMember<T> {}
+    public final Root<T> root;
+
+    private DeviceSchema(Class<T> clazz,Consumer<Builder<T>> schemaBuilder) {
+        this.root = new Root<>(this, clazz, new ArrayList<>());
+        schemaBuilder.accept(this.root);
+    }
 
     public static <T extends NonMappableIface> DeviceSchema<T> of(Class<T> clazz, Consumer<Builder<T>> schemaBuilder) {
-        DeviceSchema<T> deviceSchema = new DeviceSchema<>(clazz);
-        schemaBuilder.accept(deviceSchema.root);
-        return deviceSchema;
+        return  new DeviceSchema<>(clazz,schemaBuilder);
     }
+
+    public <B extends C99HATKernelBuilder<B>> Root<T> typedef(B builder) {
+        builder.typedefStruct(root.clazz(), _ ->
+                root.members().forEach(m -> builder
+                        .either(IfaceValue.class.isAssignableFrom(m.clazz()),
+                                _ -> builder.suffix_t(m.clazz().getSimpleName()),
+                                _ -> builder.type(m.clazz().getSimpleName())
+                        )
+                        .sp().id(m.name())
+                        .when(m instanceof DeviceSchema.Array,
+                                _ ->builder.sbrace(_ -> builder.intConst(((DeviceSchema.Array<?>) m).size()))
+                        ).semicolon().nl()
+                )
+        );
+        return root;
+    }
+
+    public static  <T extends NonMappableIface> DeviceSchema<T> getDeviceSchemaOrThrow(Class<NonMappableIface> clazz) {
+        try {
+            var schemaField = clazz.getDeclaredField("deviceSchema");
+            schemaField.setAccessible(true);
+            if (schemaField.get(schemaField) instanceof DeviceSchema<?> deviceSchema) {
+                return (DeviceSchema<T>) deviceSchema;
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        throw new RuntimeException("No DeviceSchema in "+clazz);
+    }
+
 }
