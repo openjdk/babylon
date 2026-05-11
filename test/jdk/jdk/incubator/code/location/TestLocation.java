49c13aeee6d75713fd00452910ea4df6b87ce29a
Mourad Abbay <mourad.abbay@oracle.com>
2026-05-11 22:44:37 +0100

    Assert on the Location.sourceRef value

--- test/jdk/jdk/incubator/code/location/TestLocation.java
+++ test/jdk/jdk/incubator/code/location/TestLocation.java
@@ -39,7 +39,6 @@ import org.junit.jupiter.api.Test;
 
 import java.io.StringWriter;
 import java.lang.reflect.Method;
-import java.nio.file.Path;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -47,8 +46,7 @@ public class TestLocation {
     @Test
     void testLocationIsRelative() {
         CoreOp.FuncOp f = getFuncOp(this.getClass(), "f");
-        Assertions.assertFalse(Path.of(f.location().sourceRef()).isAbsolute());
-        System.out.println(f.location().sourceRef());
+        Assertions.assertEquals("TestLocation.java", f.location().sourceRef());
     }
 
     @Test
