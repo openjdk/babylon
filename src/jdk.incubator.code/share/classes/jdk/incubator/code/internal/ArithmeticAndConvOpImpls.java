01731d6c0f2ee032aa2f9f2049a6e17197327b56
Adam Sotona <asotona@openjdk.org>
2026-05-15 12:56:16 +0200

    reversion of boolean conversions removal

--- src/jdk.incubator.code/share/classes/jdk/incubator/code/internal/ArithmeticAndConvOpImpls.java
+++ src/jdk.incubator.code/share/classes/jdk/incubator/code/internal/ArithmeticAndConvOpImpls.java
@@ -638,6 +638,9 @@ public final class ArithmeticAndConvOpImpls {
     public static byte conv_byte(double i) {
         return (byte) i;
     }
+    public static boolean conv_boolean(double i) {
+        return ((int)i & 1) == 1;
+    }
 
     // float conversion
     public static double conv_double(float i) {
@@ -660,6 +663,9 @@ public final class ArithmeticAndConvOpImpls {
     }
     public static byte conv_byte(float i) {
         return (byte) i;
+    }
+    public static boolean conv_boolean(float i) {
+        return ((int)i & 1) == 1;
     }
 
     // long conversion
@@ -684,6 +690,9 @@ public final class ArithmeticAndConvOpImpls {
     public static byte conv_byte(long i) {
         return (byte) i;
     }
+    public static boolean conv_boolean(long i) {
+        return (i & 1) == 1;
+    }
 
     // int conversion
     public static double conv_double(int i) {
@@ -706,6 +715,9 @@ public final class ArithmeticAndConvOpImpls {
     }
     public static byte conv_byte(int i) {
         return (byte) i;
+    }
+    public static boolean conv_boolean(int i) {
+        return (i & 1) == 1;
     }
 
     // short conversion
@@ -730,6 +742,9 @@ public final class ArithmeticAndConvOpImpls {
     public static byte conv_byte(short i) {
         return (byte) i;
     }
+    public static boolean conv_boolean(short i) {
+        return (i & 1) == 1;
+    }
 
     // char conversion
     public static double conv_double(char i) {
@@ -752,6 +767,9 @@ public final class ArithmeticAndConvOpImpls {
     }
     public static byte conv_byte(char i) {
         return (byte) i;
+    }
+    public static boolean conv_boolean(char i) {
+        return (i & 1) == 1;
     }
 
     // byte conversion
@@ -776,8 +794,32 @@ public final class ArithmeticAndConvOpImpls {
     public static byte conv_byte(byte i) {
         return i;
     }
+    public static boolean conv_boolean(byte i) {
+        return (i & 1) == 1;
+    }
 
     // boolean conversion
+    public static double conv_double(boolean i) {
+        return i ? 1d : 0d;
+    }
+    public static float conv_float(boolean i) {
+        return i ? 1f : 0f;
+    }
+    public static long conv_long(boolean i) {
+        return i ? 1l : 0l;
+    }
+    public static int conv_int(boolean i) {
+        return i ? 1 : 0;
+    }
+    public static short conv_short(boolean i) {
+        return i ? (short)1 : 0;
+    }
+    public static char conv_char(boolean i) {
+        return i ? (char)1 : 0;
+    }
+    public static byte conv_byte(boolean i) {
+        return i ? (byte)1 : 0;
+    }
     public static boolean conv_boolean(boolean i) {
         return i;
     }
