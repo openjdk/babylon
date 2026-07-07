ad016b8b3caf3aaf7045ccbe33c479d831da2f80
Paul Sandoz <paul.d.sandoz@googlemail.com>
2026-07-07 10:43:18 -0700

    Catch UnsupportedASTException

--- src/jdk.incubator.code/share/classes/jdk/incubator/code/internal/ReflectMethods.java
+++ src/jdk.incubator.code/share/classes/jdk/incubator/code/internal/ReflectMethods.java
@@ -219,7 +219,7 @@ public class ReflectMethods extends TreeTranslatorPrev {
                 CoreOp.FuncOp funcOp;
                 try {
                     funcOp = bodyScanner.scanMethod();
-                } catch (Exception e) {
+                } catch (UnsupportedASTException e) {
                     log.warning(tree, Warnings.ReflectableMethodUnsupported(currentClassSym.enclClass(), e.toString()));
                     super.visitMethodDef(tree);
                     return;
@@ -315,7 +315,7 @@ public class ReflectMethods extends TreeTranslatorPrev {
             CoreOp.FuncOp funcOp;
             try {
                 funcOp = bodyScanner.scanLambda();
-            } catch (Exception e) {
+            } catch (UnsupportedASTException e) {
                 log.warning(tree, Warnings.ReflectableLambdaUnsupported(currentClassSym.enclClass(), e.toString()));
                 super.visitLambda(tree);
                 return;
