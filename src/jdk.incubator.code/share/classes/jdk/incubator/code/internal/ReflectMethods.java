a21131f2c97f3c398134bd3de9dc9900165a1008
Mourad Abbay <mourad.abbay@oracle.com>
2026-08-04 04:28:58 +0100

    Move the logic that add the cast to visitApply

--- src/jdk.incubator.code/share/classes/jdk/incubator/code/internal/ReflectMethods.java
+++ src/jdk.incubator.code/share/classes/jdk/incubator/code/internal/ReflectMethods.java
@@ -742,15 +742,9 @@ public class ReflectMethods extends TreeTranslatorPrev {
         }
 
         Value coerce(Value sourceValue, Type sourceType, Type targetType) {
-            if (sourceType.isReference() && targetType.isReference()) {
-                if (!types.isSubtype(types.erasure(sourceType), types.erasure(targetType))) {
-                    return append(JavaOp.cast(typeToCodeType(targetType), sourceValue));
-                }
-                Type sourceValueType = codeTypeToType(sourceValue.type());
-                if (sourceValueType != Type.noType
-                        && !types.isSubtype(types.erasure(sourceValueType), types.erasure(targetType))) {
-                    return append(JavaOp.cast(typeToCodeType(targetType), sourceValue));
-                }
+            if (sourceType.isReference() && targetType.isReference() &&
+                    !types.isSubtype(types.erasure(sourceType), types.erasure(targetType))) {
+                return append(JavaOp.cast(typeToCodeType(targetType), sourceValue));
             }
             return convert(sourceValue, targetType);
         }
@@ -1163,6 +1157,9 @@ public class ReflectMethods extends TreeTranslatorPrev {
                     break;
                 }
                 case SELECT: {
+                    // we need this for cases like array.clone()
+                    // where tree.type is Object[] although the method return type is Object
+                    tree.type = tree.meth.type.getReturnType();
                     JCFieldAccess access = (JCFieldAccess) meth;
 
                     Type qualifierTarget = qualifierTarget(access);
