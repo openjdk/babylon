abaa89421f5f9554fd8e1f8e30506ab106755c4a
Paul Sandoz <paul.d.sandoz@googlemail.com>
2026-08-12 12:59:39 -0700

    Change variable name

--- src/jdk.incubator.code/share/classes/jdk/incubator/code/internal/ReflectMethods.java
+++ src/jdk.incubator.code/share/classes/jdk/incubator/code/internal/ReflectMethods.java
@@ -1196,9 +1196,9 @@ public class ReflectMethods extends TreeTranslatorPrev {
                     // Use the actual type of the expression, tree.type, rather than meth.type.getReturnType()
                     // This ensures invocation expressions to clone on arrays and getClass are modeled
                     // with the correct result type
-                    JavaType returnType = typeToCodeType(tree.type);//meth.type.getReturnType());
+                    JavaType resultType = typeToCodeType(tree.type);//meth.type.getReturnType());
                     JavaOp.InvokeOp iop = JavaOp.invoke(ik, tree.varargsElement != null,
-                            returnType, mr, args);
+                            resultType, mr, args);
                     Value res = append(iop);
                     if (sym.type.getReturnType().getTag() != TypeTag.VOID) {
                         result = res;
