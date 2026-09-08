7810fa6ac09b0075d9322a53f41f3ec2a75411fc
Juan Fumero <jfumero@openjdk.org>
2026-09-08 12:32:25 +0200

    HAT Format fix

--- hat/optkl/src/main/java/optkl/ifacemapper/BoundSchema.java
+++ hat/optkl/src/main/java/optkl/ifacemapper/BoundSchema.java
@@ -261,17 +261,15 @@ public class BoundSchema<T extends MappableIface> {
 
     }
 
-    static public <T extends Buffer> BoundSchema<T> of (ArenaAndLookupCarrier arenaAndLookupCarrier, Schema<T> schema, int... boundLengths){
-        return new BoundSchema<>(arenaAndLookupCarrier,schema, boundLengths);
+    public static <T extends Buffer> BoundSchema<T> of(ArenaAndLookupCarrier arenaAndLookupCarrier, Schema<T> schema, int... boundLengths) {
+        return new BoundSchema<>(arenaAndLookupCarrier, schema, boundLengths);
     }
 
-
-      public T allocate() {
+    public T allocate() {
         var segmentMapper = SegmentMapper.of(arenaAndLookupCarrier.arena(), arenaAndLookupCarrier.lookup(), schema.iface, groupLayout, this);
-        T instance =  segmentMapper.allocate(this);
+        T instance = segmentMapper.allocate(this);
         MemorySegment memorySegment = MappableIface.getMemorySegment(instance);
         int[] count = new int[]{0};
-
 
         boundArrayFields().forEach(boundArrayFieldLayout -> {
             boundArrayFieldLayout.dimFields.forEach(dimLayout -> {
@@ -291,7 +289,6 @@ public class BoundSchema<T extends MappableIface> {
                 }
             });
         });
-
 
         return instance;
     }
