a2a8fa5067a0f6713909563fe954d25568c5aab7
Juan Fumero <jfumero@openjdk.org>
2026-09-15 09:04:43 +0200

    Remove unused operations from the Tile record

--- hat/core/src/main/java/hat/types/Tile.java
+++ hat/core/src/main/java/hat/types/Tile.java
@@ -24,7 +24,6 @@
  */
 package hat.types;
 
-import jdk.incubator.code.CodeType;
 import optkl.IfaceValue;
 
 /**
@@ -32,24 +31,4 @@ import optkl.IfaceValue;
  */
 public record Tile() implements IfaceValue {
 
-    /**
-     * Transpose an input tile (swap first and second dimensions of the tile shape.
-     * @return
-     *    Transposed tile
-     */
-    public Tile transpose() {
-        return new Tile();
-    }
-
-    /**
-     * Transform an input tile to type dType.
-     *
-     * @param dType
-     *    Type to be transformed
-     * @return
-     *    A new Tile of type dType.
-     */
-    public Tile asType(CodeType dType) {
-        return new Tile();
-    }
 }
