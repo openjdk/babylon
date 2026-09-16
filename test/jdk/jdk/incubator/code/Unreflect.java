1ae7424179b23e4b1f6699e548b958392e2bfabc
Adam Sotona <asotona@openjdk.org>
2026-09-16 09:02:05 +0000

    CodeModelBootstraps implementation
    Reviewed-by: psandoz

--- test/jdk/jdk/incubator/code/Unreflect.java
+++ test/jdk/jdk/incubator/code/Unreflect.java
@@ -34,37 +34,31 @@ import java.lang.constant.ClassDesc;
 import java.lang.constant.ConstantDesc;
 import java.lang.constant.ConstantDescs;
 import java.lang.constant.DirectMethodHandleDesc;
+import java.lang.constant.DirectMethodHandleDesc.Kind;
 import java.lang.constant.DynamicCallSiteDesc;
 import java.lang.constant.MethodHandleDesc;
 import java.lang.constant.MethodTypeDesc;
-import java.lang.invoke.CallSite;
-import java.lang.invoke.ConstantCallSite;
-import java.lang.invoke.LambdaConversionException;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
 import java.lang.reflect.AccessFlag;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
-import java.util.stream.Stream;
 
-import jdk.incubator.code.CodeTransformer;
-import jdk.incubator.code.Op;
 import jdk.incubator.code.Reflect;
-import jdk.incubator.code.bytecode.BytecodeGenerator;
-import jdk.incubator.code.dialect.core.CoreOp;
-import jdk.incubator.code.dialect.core.CoreType;
-import jdk.incubator.code.dialect.java.JavaOp;
+import jdk.incubator.code.Op;
+import jdk.incubator.code.dialect.java.JavaType;
+import jdk.incubator.code.dialect.java.MethodRef;
+import jdk.incubator.code.runtime.CodeModelBootstraps;
 import jdk.incubator.code.runtime.ReflectableLambdaMetafactory;
 
 public final class Unreflect {
 
     static final ClassDesc CD_Reflect = Reflect.class.describeConstable().get();
-    static final ClassDesc CD_Unreflect = Unreflect.class.describeConstable().get();
+    static final ClassDesc CD_CodeModelBootstraps = CodeModelBootstraps.class.describeConstable().get();
     static final ClassDesc CD_ReflectableLambdaMetafactory = ReflectableLambdaMetafactory.class.describeConstable().get();
+    static final ClassDesc CD_CallerSensitive = ClassDesc.of("jdk.internal.reflect.CallerSensitive");
+
+    static final DirectMethodHandleDesc DMHD_codeModelExecutor = ConstantDescs.ofCallsiteBootstrap(
+            CD_CodeModelBootstraps, "codeModelExecutor", ConstantDescs.CD_CallSite, ConstantDescs.CD_MethodHandle);
 
     static boolean isReflective(MethodModel mm) {
         return mm.findAttribute(Attributes.runtimeVisibleAnnotations())
@@ -73,9 +67,18 @@ public final class Unreflect {
     }
 
     static byte[] transform(ClassModel clm) {
+        Set<String> modelAccessors = new HashSet<>();
+        MethodTypeDesc modelType = MethodTypeDesc.of(Op.class.describeConstable().orElseThrow());
+        clm.methods().stream().filter(m -> m.methodTypeSymbol().equals(modelType))
+                .forEach(m -> modelAccessors.add(m.methodName().stringValue()));
         return ClassFile.of(ClassFile.ConstantPoolSharingOption.NEW_POOL).transformClass(clm, (clb, cle) -> {
             if (cle instanceof MethodModel mm) {
-                if (isReflective(mm)) {
+                if (mm.methodName().equalsString("<init>") || mm.code().isEmpty()
+                        || mm.findAttribute(Attributes.runtimeVisibleAnnotations())
+                                .map(a -> a.annotations().stream().anyMatch(ann -> ann.classSymbol().equals(CD_CallerSensitive)))
+                                .orElse(false)) {
+                    clb.with(mm);
+                } else if (isReflective(mm) || modelAccessors.contains(modelAccessorName(clm, mm))) {
                     clb.transformMethod(mm, MethodTransform.dropping(me -> me instanceof CodeModel)
                             .andThen(MethodTransform.endHandler(mb -> mb.withCode(cob -> {
                                 MethodTypeDesc mts = mm.methodTypeSymbol();
@@ -86,10 +89,19 @@ public final class Unreflect {
                                 for (int i = 0; i < mts.parameterCount(); i++) {
                                     cob.loadLocal(TypeKind.from(mts.parameterType(i)), cob.parameterSlot(i));
                                 }
-                                cob.invokedynamic(DynamicCallSiteDesc.of(
-                                        ConstantDescs.ofCallsiteBootstrap(CD_Unreflect, "unreflect", ConstantDescs.CD_CallSite),
-                                        mm.methodName().stringValue(),
-                                        hasReceiver ? mts.insertParameterTypes(0, clm.thisClass().asSymbol()) : mts));
+                                boolean isInterface = clm.flags().has(AccessFlag.INTERFACE);
+                                Kind kind = hasReceiver
+                                        ? (isInterface ? Kind.INTERFACE_VIRTUAL : Kind.VIRTUAL)
+                                        : (isInterface ? Kind.INTERFACE_STATIC : Kind.STATIC);
+                                cob.invokedynamic(DynamicCallSiteDesc.of(DMHD_codeModelExecutor,
+                                                                         ConstantDescs.DEFAULT_NAME,
+                                                                         hasReceiver
+                                                                                 ? mts.insertParameterTypes(0, clm.thisClass().asSymbol())
+                                                                                 : mts,
+                                                                         MethodHandleDesc.ofMethod(kind,
+                                                                                                   clm.thisClass().asSymbol(),
+                                                                                                   mm.methodName().stringValue(),
+                                                                                                   mts)));
                                 cob.return_(TypeKind.from(mts.returnType()));
                             }))));
                 } else {
@@ -98,14 +110,15 @@ public final class Unreflect {
                         if (coe instanceof InvokeDynamicInstruction i
                                 && (bsm = i.bootstrapMethod()).owner().equals(CD_ReflectableLambdaMetafactory)) {
                             // redirect metafactory and altMetafactory
-                            cob.invokedynamic(DynamicCallSiteDesc.of(
-                                    MethodHandleDesc.ofMethod(DirectMethodHandleDesc.Kind.STATIC,
-                                                              CD_Unreflect,
-                                                              bsm.methodName(),
+                            cob.invokedynamic(DynamicCallSiteDesc.of(MethodHandleDesc.ofMethod(Kind.STATIC,
+                                                              CD_CodeModelBootstraps,
+                                                              bsm.methodName().equals("metafactory")
+                                                                      ? "lambdaMetafactory"
+                                                                      : "lambdaAltMetafactory",
                                                               bsm.invocationType()),
-                                    i.name().stringValue(),
-                                    MethodTypeDesc.ofDescriptor(i.type().stringValue()),
-                                    i.bootstrapArgs().toArray(ConstantDesc[]::new)));
+                                              i.name().stringValue(),
+                                              MethodTypeDesc.ofDescriptor(i.type().stringValue()),
+                                              i.bootstrapArgs().toArray(ConstantDesc[]::new)));
                         } else {
                             cob.with(coe);
                         }
@@ -117,84 +130,30 @@ public final class Unreflect {
         });
     }
 
-    public static CallSite unreflect(MethodHandles.Lookup caller,
-                                     String methodName,
-                                     MethodType methodType) throws NoSuchMethodException {
-        for (Method m : caller.lookupClass().getDeclaredMethods()) {
-            int firstParam = (m.getModifiers() & Modifier.STATIC) == 0 ? 1 : 0;
-            if (m.getName().equals(methodName)
-                    && m.getReturnType() == methodType.returnType()
-                    && m.getParameterCount() == methodType.parameterCount() - firstParam
-                    && Arrays.equals(m.getParameterTypes(), 0, m.getParameterCount(),
-                                     methodType.parameterArray(), firstParam, methodType.parameterCount())) {
-                return new ConstantCallSite(BytecodeGenerator.generate(caller, Op.ofMethod(m).orElseThrow()));
-            }
-        }
-        throw new NoSuchMethodException(caller.lookupClass().getName() + "." + methodName + methodType);
-    }
-
-    public static CallSite metafactory(MethodHandles.Lookup caller,
-                                       String interfaceMethodName,
-                                       MethodType factoryType,
-                                       MethodType interfaceMethodType,
-                                       MethodHandle implementation,
-                                       MethodType dynamicMethodType) throws LambdaConversionException {
-        return ReflectableLambdaMetafactory.metafactory(caller,
-                                                        interfaceMethodName,
-                                                        factoryType,
-                                                        interfaceMethodType,
-                                                        unreflectLambdaImplementation(caller, interfaceMethodName),
-                                                        dynamicMethodType);
-    }
-
-    public static CallSite altMetafactory(MethodHandles.Lookup caller,
-                                          String interfaceMethodName,
-                                          MethodType factoryType,
-                                          Object... args) throws LambdaConversionException {
-        args[1] = unreflectLambdaImplementation(caller, interfaceMethodName);
-        return ReflectableLambdaMetafactory.altMetafactory(caller,
-                                                           interfaceMethodName,
-                                                           factoryType,
-                                                           args);
-    }
-
-    static MethodHandle unreflectLambdaImplementation(MethodHandles.Lookup caller, String interfaceMethodName)
-            throws LambdaConversionException {
-        try {
-            MethodHandle opHandle = caller.findStatic(caller.lookupClass(),
-                                                      interfaceMethodName.split("=")[1],
-                                                      MethodType.methodType(Op.class));
-            return BytecodeGenerator.generate(caller, unquoteLambda((CoreOp.FuncOp)opHandle.invoke()));
-        } catch (Throwable t) {
-            throw new LambdaConversionException(t);
-        }
-    }
-
-    // flat QuotedOp and LambdaOp
-    static CoreOp.FuncOp unquoteLambda(CoreOp.FuncOp funcOp) {
-        int capturedValues = funcOp.parameters().size();
-        List<Op> ops = funcOp.body().entryBlock().ops();
-        JavaOp.LambdaOp lambda = (JavaOp.LambdaOp)((CoreOp.QuotedOp)ops.get(ops.size() - 2)).quotedOp();
-        return CoreOp.func(funcOp.funcName(), CoreType.functionType(
-                lambda.body().yieldType(),
-                Stream.of(funcOp.invokableSignature().parameterTypes(),
-                          lambda.invokableSignature().parameterTypes()).flatMap(List::stream).toList())).body(bb -> {
-            bb.context().mapBlock(funcOp.body().entryBlock(), bb.entryBlock());
-            bb.context().mapValues(funcOp.parameters(), bb.parameters().subList(0, capturedValues));
-            for (int i = 0; i < ops.size() - 2; i++) {
-                Op o = ops.get(i);
-                bb.context().mapValue(o.result(), bb.add(o));
-            }
-            bb.transformBody(lambda.body(),
-                    bb.parameters().subList(capturedValues, bb.parameters().size()),
-                    bb.context(),
-                    CodeTransformer.COPYING_TRANSFORMER);
-        });
+    static String modelAccessorName(ClassModel clm, MethodModel mm) {
+        MethodTypeDesc type = mm.methodTypeSymbol();
+        return MethodRef.method(JavaType.type(clm.thisClass().asSymbol()), mm.methodName().stringValue(),
+                JavaType.type(type.returnType()), type.parameterList().stream().map(JavaType::type).toList())
+                .toString().replace('.', '$').replace(';', '$').replace('[', '$').replace('/', '$');
     }
 
     public static void main(String[] args) throws Exception {
         // process class files from arguments
-        var toUnreflect = new ArrayDeque<>(List.of(args));
+        var toUnreflect = new ArrayDeque<String>();
+        for (String arg : args) {
+            Path path = Path.of(arg);
+            if (Files.isDirectory(path)) {
+                try (var files = Files.walk(path)) {
+                    for (Path file : files.filter(Files::isRegularFile)
+                            .filter(p -> p.toString().endsWith(".class")).toList()) {
+                        System.out.println("unreflecting " + file);
+                        Files.write(file, transform(ClassFile.of().parse(Files.readAllBytes(file))));
+                    }
+                }
+            } else {
+                toUnreflect.add(arg);
+            }
+        }
         var done = new HashSet<String>();
         while (!toUnreflect.isEmpty()) {
             String arg = toUnreflect.pop();
